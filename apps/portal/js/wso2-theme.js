abc3f0d489caec3c986201541193b0c7cf05819a
nipunamarcus <nipunamarcus2@gmail.com>
2015-10-28 09:20:03 +0530

    Updated the noty message box theme

--- apps/portal/js/wso2-theme.js
+++ apps/portal/js/wso2-theme.js
@@ -13,45 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 $.noty.themes.wso2 = {
     name: 'wso2',
-    helpers: {
-        borderFix: function () {
-            if (this.options.dismissQueue) {
-                var selector = this.options.layout.container.selector + ' ' + this.options.layout.parent.selector;
-                switch (this.options.layout.name) {
-                    case 'top':
-                        $(selector).css({borderRadius: '0px 0px 0px 0px'});
-                        $(selector).last().css({borderRadius: '0px 0px 5px 5px'});
-                        break;
-                    case 'topCenter':
-                    case 'topLeft':
-                    case 'topRight':
-                    case 'bottomCenter':
-                    case 'bottomLeft':
-                    case 'bottomRight':
-                    case 'center':
-                    case 'centerLeft':
-                    case 'centerRight':
-                    case 'inline':
-                        $(selector).css({borderRadius: '0px 0px 0px 0px'});
-                        $(selector).first().css({'border-top-left-radius': '5px', 'border-top-right-radius': '5px'});
-                        $(selector).last().css({
-                            'border-bottom-left-radius': '5px',
-                            'border-bottom-right-radius': '5px'
-                        });
-                        break;
-                    case 'bottom':
-                        $(selector).css({borderRadius: '0px 0px 0px 0px'});
-                        $(selector).first().css({borderRadius: '5px 5px 0px 0px'});
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    },
+    helpers: {},
     modal: {
         css: {
             position: 'fixed',
@@ -69,15 +33,15 @@ $.noty.themes.wso2 = {
 
         this.$bar.css({
             overflow: 'hidden',
-            background: "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABsAAAAoCAQAAAClM0ndAAAAhklEQVR4AdXO0QrCMBBE0bttkk38/w8WRERpdyjzVOc+HxhIHqJGMQcFFkpYRQotLLSw0IJ5aBdovruMYDA/kT8plF9ZKLFQcgF18hDj1SbQOMlCA4kao0iiXmah7qBWPdxpohsgVZyj7e5I9KcID+EhiDI5gxBYKLBQYKHAQoGFAoEks/YEGHYKB7hFxf0AAAAASUVORK5CYII=') repeat-x scroll left top #fff"
+            margin: '4px 0',
+            borderRadius: '2px'
         });
 
         this.$message.css({
-            fontSize: '13px',
+            fontSize: '14px',
             lineHeight: '16px',
-            fontWeight: 'bold',
             textAlign: 'center',
-            padding: '8px 10px 9px',
+            padding: '10px',
             width: 'auto',
             position: 'relative'
         });
@@ -88,8 +52,7 @@ $.noty.themes.wso2 = {
             width: 10, height: 10,
             background: "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAQAAAAnOwc2AAAAxUlEQVR4AR3MPUoDURSA0e++uSkkOxC3IAOWNtaCIDaChfgXBMEZbQRByxCwk+BasgQRZLSYoLgDQbARxry8nyumPcVRKDfd0Aa8AsgDv1zp6pYd5jWOwhvebRTbzNNEw5BSsIpsj/kurQBnmk7sIFcCF5yyZPDRG6trQhujXYosaFoc+2f1MJ89uc76IND6F9BvlXUdpb6xwD2+4q3me3bysiHvtLYrUJto7PD/ve7LNHxSg/woN2kSz4txasBdhyiz3ugPGetTjm3XRokAAAAASUVORK5CYII=)",
             display: 'none',
-            cursor: 'pointer',
-            opacity: 1
+            cursor: 'pointer'
         });
 
         this.$buttons.css({
@@ -119,10 +82,10 @@ $.noty.themes.wso2 = {
         switch (this.options.layout.name) {
             case 'top':
                 this.$bar.css({
-                    borderRadius: '0px 0px 5px 5px',
                     borderBottom: '2px solid #eee',
                     borderLeft: '2px solid #eee',
                     borderRight: '2px solid #eee',
+                    borderTop: '2px solid #eee',
                     boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)"
                 });
                 break;
@@ -131,7 +94,6 @@ $.noty.themes.wso2 = {
             case 'bottomCenter':
             case 'inline':
                 this.$bar.css({
-                    borderRadius: '5px',
                     border: '1px solid #eee',
                     boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)"
                 });
@@ -144,7 +106,6 @@ $.noty.themes.wso2 = {
             case 'centerLeft':
             case 'centerRight':
                 this.$bar.css({
-                    borderRadius: '5px',
                     border: '1px solid #eee',
                     boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)"
                 });
@@ -152,10 +113,10 @@ $.noty.themes.wso2 = {
                 break;
             case 'bottom':
                 this.$bar.css({
-                    borderRadius: '5px 5px 0px 0px',
                     borderTop: '2px solid #eee',
                     borderLeft: '2px solid #eee',
                     borderRight: '2px solid #eee',
+                    borderBottom: '2px solid #eee',
                     boxShadow: "0 -2px 4px rgba(0, 0, 0, 0.1)"
                 });
                 break;
@@ -170,36 +131,36 @@ $.noty.themes.wso2 = {
         switch (this.options.type) {
             case 'alert':
             case 'notification':
-                this.$bar.css({backgroundColor: '#FFF', borderColor: '#CCC', color: '#444'});
+                this.$bar.css({backgroundColor: '#FFF', borderColor: '#dedede', color: '#444'});
                 break;
             case 'warning':
                 this.$bar.css({backgroundColor: '#FFEAA8', borderColor: '#FFC237', color: '#826200'});
                 this.$buttons.css({borderTop: '1px solid #FFC237'});
                 break;
             case 'error':
-                this.$bar.css({backgroundColor: '#E74C3C', borderColor: 'darkred', color: '#FFF'});
+                this.$bar.css({backgroundColor: '#e74c3c', borderColor: '#e74c3c', color: '#FFF'});
                 this.$message.css({fontWeight: 'bold'});
                 this.$buttons.css({borderTop: '1px solid darkred'});
                 break;
             case 'information':
-                this.$bar.css({backgroundColor: '#57B7E2', borderColor: '#0B90C4', color: '#FFF'});
+                this.$bar.css({backgroundColor: '#78C5E7', borderColor: '#3badd6', color: '#FFF'});
                 this.$buttons.css({borderTop: '1px solid #0B90C4'});
                 break;
             case 'success':
-                this.$bar.css({backgroundColor: 'lightgreen', borderColor: '#50C24E', color: 'darkgreen'});
+                this.$bar.css({backgroundColor: '#40D47E', borderColor: '#40D47E', color: '#fff'});
                 this.$buttons.css({borderTop: '1px solid #50C24E'});
                 break;
             default:
-                this.$bar.css({backgroundColor: '#FFF', borderColor: '#CCC', color: '#444'});
+                this.$bar.css({backgroundColor: '#464646', borderColor: '#464646', color: '#fff'});
                 break;
         }
     },
     callback: {
         onShow: function () {
-            $.noty.themes.wso2.helpers.borderFix.apply(this);
+
         },
         onClose: function () {
-            $.noty.themes.wso2.helpers.borderFix.apply(this);
+
         }
     }
 };
