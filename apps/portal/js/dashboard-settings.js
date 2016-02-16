322444e42da55434468e05dbd662690569e52475
nipunamarcus <nipunamarcus2@gmail.com>
2016-02-16 19:49:12 +0530

    Fix the issue where getting a error when removing role from permission which is not belong to the user's roles

--- apps/portal/js/dashboard-settings.js
+++ apps/portal/js/dashboard-settings.js
@@ -51,7 +51,7 @@ $(function () {
             });
         });
     };
-    
+
     /**
      * Generate Noty Messages as to the content given using parameters.
      * @param1 text {String}
@@ -70,19 +70,19 @@ $(function () {
             properties.buttons = [
                 {
                     addClass: 'btn btn-primary', text: 'Ok', onClick: function ($noty) {
-                        $noty.close();
-                        if (ok) {
-                            ok();
-                        }
+                    $noty.close();
+                    if (ok) {
+                        ok();
                     }
+                }
                 },
                 {
                     addClass: 'btn btn-danger', text: 'Cancel', onClick: function ($noty) {
-                        $noty.close();
-                        if (cancel) {
-                            cancel();
-                        }
+                    $noty.close();
+                    if (cancel) {
+                        cancel();
                     }
+                }
                 }
             ];
         }
@@ -342,6 +342,7 @@ $(function () {
         return userRoles;
     };
 
+
     /**
      * Initialize the UI functionality.
      * @private
@@ -494,7 +495,9 @@ $(function () {
                 saveDashboard(removeElement);
             };
 
-            if ((editors.length == 1 || getNumberOfUserRolesInDashboard(editors) == 1) && !user.isAdmin) {
+            if ((editors.length == 1 || (getNumberOfUserRolesInDashboard(editors) == 1
+                && isExistingPermission(user.roles, role)))
+                && !user.isAdmin) {
                 showConfirm("Removing Permission",
                     "After this permission removal only administrator will be able to edit this dashboard." +
                     " Do you want to continue?", removePermission);
@@ -512,7 +515,9 @@ $(function () {
                 saveDashboard(removeElement);
             };
 
-            if ((viewers.length == 1 || getNumberOfUserRolesInDashboard(viewers) == 1) && !user.isAdmin) {
+            if ((viewers.length == 1 || (getNumberOfUserRolesInDashboard(viewers) == 1
+                && isExistingPermission(user.roles, role)))
+                && !user.isAdmin) {
                 showConfirm("Removing Permission",
                     "After this permission removal only administrator will be able to view this dashboard." +
                     " Do you want to continue?", removePermission);
