1ac7feecc20cf054c6b29b4d5ee95aa1f7d2be4e
Damith <damith@damith-Latitude-E6540>
2016-03-01 17:08:45 +0530

    removed duplicates

--- apps/portal/templates/gadgets/igviz/js/main.js
+++ apps/portal/templates/gadgets/igviz/js/main.js
@@ -250,7 +250,7 @@ function createDatatable(data) {
 
         var type = columns[i]["type"].toUpperCase();
 
-        if(type === "INT" || type === "INTEGER" || type === "FLOAT" || type === "LONG" ||
+        if(type === "INT" || type === "INTEGER" || type === "FLOAT" ||
             type === "DOUBLE" || type === "LONG") {
             type = "linear";
         } else if (gadgetConfig.columns[i]["type"].toUpperCase() == "TIME") {
