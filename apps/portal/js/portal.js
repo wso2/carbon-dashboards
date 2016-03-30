2c3a60288bb017cb5fb6cf220fb156cb9defa290
nipunamarcus <nipunamarcus2@gmail.com>
2016-03-30 14:54:32 +0530

    Updated the licence for files

--- apps/portal/js/portal.js
+++ apps/portal/js/portal.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 WSO2, Inc. (http://wso2.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 $(function () {
     var dashboardsApi = ues.utils.tenantPrefix() + 'apis/dashboards';
     var dashboardsListHbs = Handlebars.compile($("#ues-dashboards-list-hbs").html());
@@ -120,11 +135,11 @@ $(function () {
             var dashboard = findDashboard(id);
             dashboardEl.html(dashboardThumbnailHbs(dashboard));
         });
-        
-        portal.on('click', '.ues-view:not(.disable)', function(e) {
+
+        portal.on('click', '.ues-view:not(.disable)', function (e) {
             e.preventDefault();
             window.open($(this).attr('href'), '_blank');
-        })
+        });
 
         $(window).scroll(function () {
             var win = $(window);
