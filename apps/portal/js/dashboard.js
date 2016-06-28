8ade9df807dcb70c4776f613d90effef770a041f
Udara Rathnayake <udarakr@gmail.com>
2016-06-28 14:17:40 +0530

    enabling resize and relocate gadgets in view mode

--- apps/portal/js/dashboard.js
+++ apps/portal/js/dashboard.js
@@ -41,6 +41,14 @@ $(function () {
      * @const
      */
     var RPC_GADGET_BUTTON_CALLBACK = "RPC_GADGET_BUTTON_CALLBACK";
+
+    /**
+     * Anonymous dashboard view mode.
+     * @const
+     */
+    var ANONYMOUS_DASHBOARD_VIEW = 'anon';
+
+    var dashboardsApi = ues.utils.tenantPrefix() + 'apis/dashboards';
     
     var page;
     /**
@@ -63,6 +71,28 @@ $(function () {
             var action = $(this).attr('data-action');
             gadgets.rpc.call(fid, RPC_GADGET_BUTTON_CALLBACK, null, action);
         });
+
+        //gadget titlebar click function handler
+/*        viewer.on('click', '.ues-component-heading', function (e) {
+            console.log('ues-component-heading clicked');
+            if( $(this).hasClass('edit') ){
+                //TODO disable edit options from the dashboard view
+                $(this).removeClass('edit');
+                $(this).find('.ues-component-full-handle').show();
+                $(this).find('.ues-component-delete-handle').hide();
+
+
+            } else {
+                //TODO enable edit options in the dashboard view
+                $(this).addClass('edit');
+                //change maxmize iclass to delete
+                $(this).find('.ues-component-full-handle').hide();
+                $(this).find('.ues-component-delete-handle').show();
+
+                //debugger;
+                //
+            }
+        });*/
 
         // gadget maximization handler
         viewer.on('click', '.ues-component-full-handle', function (e) {
@@ -255,11 +285,161 @@ $(function () {
                 width: 12,
                 cellHeight: 50,
                 verticalMargin: 30,
-                disableResize: true,
-                disableDrag: true,
+                disableResize: false,
+                disableDrag: false,
+            }).on('dragstop', function (e, ui) {
+                updateLayout();
+            }).on('resizestart', function (e, ui) {
+                // hide the component content on start resizing the component
+                var container = $(ui.element).find('.ues-component');
+                if (container) {
+                    container.find('.ues-component-body').hide();
+                }
+            }).on('resizestop', function (e, ui) {
+                // re-render component on stop resizing the component
+                var container = $(ui.element).find('.ues-component');
+                if (container) {
+                    var gsItem = container.closest('.grid-stack-item');
+                    var node = gsItem.data('_gridstack_node');
+                    var gsHeight = node ? node.height : parseInt(gsItem.attr('data-gs-height'));
+                    var height = (gsHeight * 150) + ((gsHeight - 1) * 30);
+                    container.closest('.ues-component-box').attr('data-height', height);
+                    container.find('.ues-component-body').show();
+                    if (container.attr('id')) {
+                        updateComponent(container.attr('id'));
+                    }
+                }
+
+                updateLayout();
+
             });
         });
         $('.nano').nanoScroller();
+    };
+
+    /**
+     * Update the layout after modification.
+     * @return {null}
+     */
+    var updateLayout = function () {
+
+        // extract the layout from the designer and save it
+        var res = _.map($('.grid-stack .grid-stack-item:visible'), function (el) {
+            el = $(el);
+            var node = el.data('_gridstack_node');
+
+            if (node) {
+                return {
+                    id: el.attr('data-id'),
+                    x: node.x,
+                    y: node.y,
+                    width: node.width,
+                    height: node.height,
+                    banner: el.attr('data-banner') == 'true'
+                };
+            }
+        });
+
+        var serializedGrid = [];
+        for (i = 0; i < res.length; i++) {
+            if (res[i]) {
+                serializedGrid.push(res[i]);
+            }
+        }
+
+        var json = {blocks: serializedGrid};
+        var id;
+        var i;
+        var length;
+        // find the current page index
+        for (i = 0,length = ues.global.dashboard.pages.length; i < length; i++) {
+            if (ues.global.dashboard.pages[i].id === page.id) {
+                id = i;
+            }
+        }
+        if (typeof id === 'undefined') {
+            throw 'Specified page : ' + page.id + ' cannot be found';
+        }
+        if (pageType === ANONYMOUS_DASHBOARD_VIEW) {
+            ues.global.dashboard.pages[id].layout.content.anon = json;
+        } else {
+            ues.global.dashboard.pages[id].layout.content.loggedIn = json;
+        }
+        saveDashboard();
+    };
+
+    /**
+     * Saves the dashboard content.
+     * @return {null}
+     * @private
+     */
+    var saveDashboard = function () {
+        var method = 'PUT';
+        var url = dashboardsApi + '/' + dashboard.id;
+        var isRedirect = false;
+        $.ajax({
+            url: url,
+            method: method,
+            data: JSON.stringify(dashboard),
+            async: false,
+            contentType: 'application/json'
+        }).success(function (data) {
+            //generateMessage("Dashboard saved successfully", null, null, "success", "topCenter", 2000, null);
+            console.log("Dashboard saved successfully.");
+            if (isRedirect) {
+                isRedirect = false;
+                window.location = dashboardsUrl + '/' + dashboard.id + "?editor=true";
+            }
+        }).error(function (xhr, status, err) {
+            if (xhr.status === 403) {
+                window.location.reload();
+                return;
+            }
+            //("Error saving the dashboard", null, null, "error", "topCenter", 2000, null);
+            console.log("Error saving the dashboard.");
+        });
+    };
+
+    /**
+     * Triggers update hook of a given component.
+     * @param {String} id   Component ID
+     * @return {null}
+     * @private
+     */
+    var updateComponent = function (id) {
+        ues.components.update(findComponent(id), function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+    };
+
+    /**
+     * Find a given component in the current page.
+     * @param {String} id   The component ID
+     * @return {Object}
+     * @private
+     */
+    var findComponent = function (id) {
+        var i;
+        var length;
+        var area;
+        var component;
+        var components;
+        pageType = pageType ? pageType : DEFAULT_DASHBOARD_VIEW;
+        var content = page.content[pageType];
+        for (area in content) {
+            if (content.hasOwnProperty(area)) {
+                components = content[area];
+                length = components.length;
+                for (i = 0; i < length; i++) {
+                    component = components[i];
+                    if (component.id === id) {
+                        return component;
+                    }
+                }
+            }
+        }
     };
 
     initDashboard();
