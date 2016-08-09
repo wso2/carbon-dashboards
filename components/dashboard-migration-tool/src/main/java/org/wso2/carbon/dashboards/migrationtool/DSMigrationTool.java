913e39fd719b18101a75dc1a4b499a9099478364
nisalaniroshana <nisalaniroshana@gmail.com>
2016-08-09 16:27:42 +0530

    Migration Tool for DS

---
+++ components/dashboard-migration-tool/src/main/java/org/wso2/carbon/dashboards/migrationtool/DSMigrationTool.java
@@ -0,0 +1,181 @@
+/**
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * <p>
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.dashboards.migrationtool;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.BasicConfigurator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DSMigrationTool {
+    private static final Log log = LogFactory.getLog(DSMigrationTool.class);
+    private static final String GADGET_JSON = "gadget.json";
+
+    /**
+     * Update the gadgetJSON in the gadgets folders.iterate through all the gadgets and update the gadget jsons
+     *
+     * @param artifactPath artifact path which consists the gadgets folders
+     */
+    protected void gadgetJSONUpdater(File artifactPath) {
+        File[] listOfFiles = artifactPath.listFiles();
+        String modifiedGadgetID;
+        JSONObject gadgetJSONObject = null, gadgetDataObj;
+        String thumbnail;
+        String url;
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isDirectory()) {
+                try {
+                    JSONParser jsonParser = new JSONParser();
+                    Object obj = jsonParser.parse(new FileReader(
+                            artifactPath + File.separator + listOfFiles[i].getName() + File.separator + GADGET_JSON));
+                    gadgetJSONObject = (JSONObject) obj;
+                    modifiedGadgetID = listOfFiles[i].getName().toLowerCase();
+                    gadgetJSONObject.put("id", modifiedGadgetID);
+                    thumbnail = (String) gadgetJSONObject.get("thumbnail");
+                    gadgetDataObj = (JSONObject) gadgetJSONObject.get("data");
+                    url = (String) (gadgetDataObj).get("url");
+                    if ((thumbnail).contains(modifiedGadgetID)) {
+                        gadgetJSONObject.remove("thumbnail");
+                        gadgetJSONObject.put("thumbnail", replace(thumbnail, modifiedGadgetID, modifiedGadgetID));
+                    }
+                    gadgetDataObj.remove("url");
+                    if (url.toLowerCase().contains("local")) {
+                        url = replace(url, "gadget", "fs/gadget");
+                    }
+                    gadgetDataObj.put("url", replace(url, modifiedGadgetID, modifiedGadgetID));
+                    FileWriter file = new FileWriter(
+                            artifactPath + File.separator + listOfFiles[i].getName() + File.separator + GADGET_JSON);
+                    file.write(gadgetJSONObject.toJSONString());
+                    file.flush();
+                    file.close();
+                    listOfFiles[i].renameTo(new File(artifactPath + File.separator + modifiedGadgetID));
+                    log.info("Store - Gadget "+gadgetJSONObject.get("id")+" is updated successfully !");
+                } catch (IOException e) {
+                    log.error("Error in reading the gadget json "+gadgetJSONObject.get("id"));
+                } catch (ParseException e) {
+                    log.error("Error in parsing the gadget json "+gadgetJSONObject.get("id"));
+                }
+            }
+        }
+    }
+
+    /**
+     * Replace a specific word from specific text using given replacement text
+     * @param srcWord original string
+     * @param toReplace text to be replaced
+     * @param replacement replacement text
+     * @return modified text
+     */
+    protected String replace(String srcWord, String toReplace, String replacement) {
+        Pattern pattern = Pattern.compile(toReplace, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(srcWord);
+        String result = matcher.replaceFirst(replacement);
+        return result;
+    }
+
+    /**
+     * update the blocks section of dashboard json in order to compatible with carbon-dashboards version 1.0.15+
+     *
+     * @param blocksArray blocks array dashboard json
+     */
+    protected void updateDashboardBlocks(JSONArray blocksArray) {
+        JSONObject block = null;
+        long col;
+        long row;
+        long size_y;
+        long size_x;
+
+        for (int i = 0; i < blocksArray.size(); i++) {
+            block = (JSONObject) blocksArray.get(i);
+            if (block.containsKey("col")) {
+                col = (Long) block.get("col");
+                block.put("x", col - 1);
+                block.remove("col");
+            }
+            if (block.containsKey("row")) {
+                row = (Long) block.get("row");
+                block.put("y", row - 1);
+                block.remove("row");
+            }
+            if (block.containsKey("size_x")) {
+                size_x = (Long) block.get("size_x");
+                block.put("width", size_x);
+                block.remove("size_x");
+            }
+            if (block.containsKey("size_y")) {
+                size_y = (Long) block.get("size_y");
+                block.put("height", size_y);
+                block.remove("size_y");
+            }
+        }
+    }
+
+    /**
+     * update the gadgetId of gadgets in the dashboard json with the modified gadgetID
+     *
+     * @param gadgetArr gadgets Array to update
+     */
+    protected void dashboardGadgetUpdater(JSONArray gadgetArr) {
+        JSONObject gadgetContentObj;
+        String thumbnail, url;
+        String gadgetID;
+        JSONObject gadgetDataObj;
+        for (int i = 0; i < gadgetArr.size(); i++) {
+            gadgetContentObj = (JSONObject) ((JSONObject) gadgetArr.get(i)).get("content");
+            gadgetDataObj = (JSONObject) gadgetContentObj.get("data");
+            url = (String) (gadgetDataObj).get("url");
+            gadgetID = getGadgetID(url);
+            gadgetContentObj.remove("id");
+            gadgetContentObj.put("id", gadgetID);
+            thumbnail = (String) gadgetContentObj.get("thumbnail");
+            if ((thumbnail).contains(gadgetID)) {
+                gadgetContentObj.remove("thumbnail");
+                gadgetContentObj.put("thumbnail", replace(thumbnail, gadgetID, gadgetID));
+            }
+            gadgetDataObj.remove("url");
+            if (url.toLowerCase().contains("local")) {
+                url = replace(url, "gadget", "fs/gadget");
+            }
+            gadgetDataObj.put("url", replace(url, gadgetID, gadgetID));
+            log.info("Dashboard - Gadget "+gadgetContentObj.get("id")+" is successfully updated !");
+        }
+    }
+
+    /**
+     * get the modified gadgetID using thumbnail URL
+     *
+     * @param url data URL of gadget
+     * @return gadgetID
+     */
+    private String getGadgetID(String url) {
+        String[] splittedArray = url.split("/");
+        String gadgetID = splittedArray[splittedArray.length - 2];
+        return gadgetID.toLowerCase();
+    }
+
+}
---
+++ components/dashboard-migration-tool/src/main/java/org/wso2/carbon/dashboards/migrationtool/DSPortalAppMigrationTool.java
@@ -0,0 +1,258 @@
+/**
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * <p>
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.dashboards.migrationtool;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.BasicConfigurator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.cert.X509Certificate;
+
+public class DSPortalAppMigrationTool extends DSMigrationTool {
+    private static final Log log = LogFactory.getLog(DSPortalAppMigrationTool.class);
+    private static final String GADGET = "gadget";
+    private static final String WIDGET = "widget";
+    private static final String LAYOUT = "layout";
+    private static final String BLOCKS = "blocks";
+    private static final String INDEX_JSON = "index.json";
+    private static String storePath = "/repository/deployment/server/jaggeryapps/portal/store";
+    private static String username;
+    private static String password;
+    static String productHome;
+    static String destPath ;
+    static String tempDir;
+
+    public static void main(String arg[]) {
+        System.out.println(System.getProperty("srcdir")+"   V   "+System.getProperty("mode/**/"));
+        productHome = arg[0];
+        destPath = arg[1];
+        username = arg[2];
+        password = arg[3];
+        tempDir = productHome + "/tempDir";
+        BasicConfigurator.configure();
+        DSPortalAppMigrationTool dsPortalAppMigrationTool = new DSPortalAppMigrationTool();
+        dsPortalAppMigrationTool.copyDirectory(new File(productHome + storePath), new File(tempDir));
+        dsPortalAppMigrationTool.migrateArtifactsInStore();
+        dsPortalAppMigrationTool.copyDirectory(new File(tempDir), new File(destPath));
+        dsPortalAppMigrationTool.updateMigratedStoreWithStoreType(new File(destPath));
+        dsPortalAppMigrationTool.updateDashboardJSON();
+        log.info("Portal Migration is completed successfully !");
+    }
+
+    /**
+     * copy src directory to destination directory
+     *
+     * @param srcDir  source directory
+     * @param destDir destination directory
+     */
+    public void copyDirectory(File srcDir, File destDir) {
+        try {
+            FileUtils.copyDirectory(srcDir, destDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * update the given modified directory adding store type as fs
+     *
+     * @param migratedDir
+     */
+    public void updateMigratedStoreWithStoreType(File migratedDir) {
+        File[] tenantStores = migratedDir.listFiles();
+        for (int i = 0; i < tenantStores.length; i++) {
+            if (tenantStores[i].isDirectory()) {
+                File fsStoreTemp = new File(tempDir + File.separator + "fs");
+                try {
+                    FileUtils.copyDirectory(tenantStores[i], fsStoreTemp);
+                    FileUtils.deleteDirectory(tenantStores[i]);
+                    FileUtils.copyDirectory(fsStoreTemp, new File(tenantStores[i].getPath() + "/fs"));
+                    FileUtils.deleteDirectory(new File(tempDir));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    /**
+     * migrate the different types of artifacts such as gadgets,widgets and layouts into newer version
+     */
+    private void migrateArtifactsInStore() {
+        File store = new File(tempDir);
+        File[] tenantStores = store.listFiles();
+        for (int i = 0; i < tenantStores.length; i++) {
+            if (tenantStores[i].isDirectory()) {
+                File[] artifactTypes = tenantStores[i].listFiles();
+                for (int artifactCount = 0; artifactCount < artifactTypes.length; artifactCount++) {
+                    if (artifactTypes[artifactCount].getName().equalsIgnoreCase(GADGET) || artifactTypes[artifactCount]
+                            .getName().equalsIgnoreCase(WIDGET)) {
+                        new DSPortalAppMigrationTool().gadgetJSONUpdater(artifactTypes[artifactCount]);
+                    } else if (artifactTypes[artifactCount].getName().equalsIgnoreCase(LAYOUT)) {
+                        migrateLayoutsInStore(artifactTypes[artifactCount]);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * migrate layouts into newer version
+     *
+     * @param layouts file path of the layouts directory
+     */
+    private void migrateLayoutsInStore(File layouts) {
+        File[] listOflayouts = layouts.listFiles();
+        JSONParser parser = new JSONParser();
+        for (int layoutCount = 0; layoutCount < listOflayouts.length; layoutCount++) {
+            try {
+                JSONObject layoutObj = (JSONObject) parser.parse(new FileReader(
+                        listOflayouts[layoutCount].getAbsolutePath() + File.separator + INDEX_JSON));
+                updateDashboardBlocks(((JSONArray) layoutObj.get(BLOCKS)));
+                FileWriter file = new FileWriter(
+                        listOflayouts[layoutCount].getAbsolutePath() + File.separator + INDEX_JSON);
+                file.write(layoutObj.toJSONString());
+                file.flush();
+                file.close();
+
+            } catch (IOException e) {
+                log.error("Error in opening the file " + listOflayouts[layoutCount].getName(), e);
+            } catch (ParseException e) {
+                log.error("Error in parsing the index.json file in " + listOflayouts[layoutCount].getAbsolutePath());
+            }
+        }
+    }
+
+    private TrustManager[] get_trust_mgr() {
+        TrustManager[] certs = new TrustManager[] { new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String t) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String t) {
+            }
+        } };
+        return certs;
+    }
+
+    public void updateDashboardJSON() {
+        String response = invokeRestAPI("https://localhost:9443/portal/apis/login?username=admin&password=admin",
+                "POST", null, null);
+        try {
+            JSONObject responseObj = (JSONObject) new JSONParser().parse(response.toString());
+            String sessionId = (String) responseObj.get("sessionId");
+            response = invokeRestAPI("https://localhost:9443/portal/apis/dashboards", "GET", sessionId, null);
+            JSONArray responseArr = (JSONArray) new JSONParser().parse(response.toString());
+            for (int dashboardCount = 0; dashboardCount < responseArr.size(); dashboardCount++) {
+                modifyDashboardJSON(dashboardUpdater((JSONObject) responseArr.get(dashboardCount)), sessionId);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String invokeRestAPI(String requestURL, String requestType, String sessionId, String content) {
+        try {
+            System.setProperty("javax.net.ssl.trustStore",
+                    productHome + "/repository/resources/security/wso2carbon.jks");
+            System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
+
+            URL url = new URL(requestURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(requestType);
+
+            if (sessionId != null) {
+                conn.setRequestProperty("Cookie", "JSESSIONID=" + sessionId);
+            }
+            if (content != null) {
+                conn.setDoOutput(true);
+                conn.setRequestProperty("Content-Type", "application/json");
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write(content);
+                wr.flush();
+            }
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            String output;
+            StringBuilder buffer = new StringBuilder();
+            while ((output = br.readLine()) != null) {
+                buffer.append(output);
+            }
+            output = buffer.toString();
+            conn.disconnect();
+            return output;
+        } catch (MalformedURLException e) {
+            log.error("Error in rest api url " + requestURL, e);
+        } catch (IOException e) {
+            log.error("Error in calling rest api url : " + requestURL, e);
+        }
+        return null;
+    }
+
+    /**
+     * update dahboard json in the ds dashboard in order to compatible with carbon-dashboards version 1.0.15+
+     *
+     * @param dashboardJSONObject dashboardJSON to be updated
+     */
+    private JSONObject dashboardUpdater(JSONObject dashboardJSONObject) {
+        Object obj = dashboardJSONObject.get("pages");
+        JSONArray pagesJSONArray = (JSONArray) obj;
+        for (int pageCount = 0; pageCount < pagesJSONArray.size(); pageCount++) {
+            JSONObject pageObj = ((JSONObject) pagesJSONArray.get(pageCount));
+            JSONArray blocksArray = ((JSONArray) ((JSONObject) ((JSONObject) ((JSONObject) pageObj.get("layout"))
+                    .get("content")).get("loggedIn")).get("blocks"));
+            JSONObject gadgetSet = ((JSONObject) ((JSONObject) pageObj.get("content")).get("default"));
+            Object[] keySet = ((JSONObject) ((JSONObject) pageObj.get("content")).get("default")).keySet().toArray();
+            for (int gadgetCount = 0; gadgetCount < keySet.length; gadgetCount++) {
+                dashboardGadgetUpdater(((JSONArray) gadgetSet.get(keySet[gadgetCount])));
+            }
+            updateDashboardBlocks(blocksArray);
+        }
+        return dashboardJSONObject;
+    }
+
+    /**
+     * Put the modified dashboard.json into registry
+     *
+     * @param dashboardObj dashboard json object
+     * @param sessionId    relevant session ID
+     */
+    private void modifyDashboardJSON(JSONObject dashboardObj, String sessionId) {
+        String response = invokeRestAPI(
+                "https://localhost:9443/portal/apis/dashboards/" + (String) dashboardObj.get("id"), "PUT", sessionId,
+                dashboardObj.toJSONString());
+    }
+}
