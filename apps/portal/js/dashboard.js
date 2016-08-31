99d777a416305e6e48f8782a004dadaf7b98f5f1
nipunamarcus <nipunamarcus2@gmail.com>
2016-08-31 20:51:11 +0530

    Refactor the hierarchical pages feature

--- apps/portal/js/dashboard.js
+++ apps/portal/js/dashboard.js
@@ -487,7 +487,7 @@ $(function () {
         }
         return false;
     };
-    
+
     //compile handlebar for the menu list
     var updateMenuList = function () {
         //menulist for big res
