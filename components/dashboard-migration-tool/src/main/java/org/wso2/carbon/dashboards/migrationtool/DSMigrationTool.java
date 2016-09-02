d0defb6efb66681dec7eca5c541ea99999f85957
nisalaniroshana <nisalaniroshana@gmail.com>
2016-09-02 15:07:56 +0530

    Fixed the issue in migrating gadgets in carbon-dashboards-1.0.11

--- components/dashboard-migration-tool/src/main/java/org/wso2/carbon/dashboards/migrationtool/DSMigrationTool.java
+++ components/dashboard-migration-tool/src/main/java/org/wso2/carbon/dashboards/migrationtool/DSMigrationTool.java
@@ -80,7 +80,8 @@ public class DSMigrationTool {
                     }
                     gadgetDataObj.remove(Constants.URL);
                     if (url.toLowerCase().contains(Constants.LOCAL)) {
-                        url = replace(url, Constants.GADGET, Constants.FS_STORE + File.separator + Constants.GADGET);
+                        url = replace(url, url.substring(0, url.indexOf(Constants.GADGET + File.separator)),
+                                Constants.GADGET + File.separator);
                     }
                     gadgetDataObj.put(Constants.URL, replace(url, modifiedGadgetID, modifiedGadgetID));
                     file = new FileWriter(artifactPath + File.separator + listOfFiles[i].getName() + File.separator
