302c00b6b61151c741fb4d966efc80429f9a129e
Subangani <subangani1993@gmail.com>
2016-09-15 11:00:23 +0530

    Edit generated gadgets and give notification before editing.

--- apps/portal/js/assets.js
+++ apps/portal/js/assets.js
@@ -36,10 +36,13 @@ $(function () {
     var assetsListHbs = Handlebars.compile($("#ds-assets-list-hbs").html());
     var assetThumbnailHbs = Handlebars.compile($("#ds-asset-thumbnail-hbs").html());
     var assetConfirmHbs = Handlebars.compile($("#ds-asset-confirm-hbs").html());
+    var editAssetConfirmHbs = Handlebars.compile($("#ds-edit-assets-confirm-hbs").html());
     var gadgetUsageConfimrHbs = Handlebars.compile($("#ds-gadget-usage-confirm-hbs").html());
+    var editGadgetUsageConfirmHbs = Handlebars.compile($("#ds-gadget-usage-edit-confirm-hbs").html());
     var assetsEmptyHbs = Handlebars.compile($("#ds-assets-empty-hbs").html());
     var assetsDeleteErrorHbs = Handlebars.compile($("#ds-asset-delete-error-hbs").html());
     var DATABASE_API = ues.utils.tenantPrefix() + 'apis/database';
+    var editGadgetUrl = ues.utils.tenantPrefix() + '/create-gadget';
     Handlebars.registerPartial('ds-asset-thumbnail-hbs', assetThumbnailHbs);
     /**
      * Load the list of assets available.
@@ -87,7 +90,7 @@ $(function () {
         ues.store.getStoreList(function (error, data) {
             if (!error && data) {
                 var length = data.length;
-                
+
                 if(length > 1){
                     for (var i = 0; i < length; i++) {
                         addStoreToList(data[i]);
@@ -95,7 +98,7 @@ $(function () {
                     $('#selectStore').show();
                 }
                 else {
-                    $('#selectStore').replaceWith('<label class="control-label">' + data[0] + '</label>');
+                    $('#selectStore').replaceWith('<label id="store" class="control-label">' + data[0] + '</label>');
                 }
                 $('#storeSelector').show();
             } else {
@@ -112,6 +115,18 @@ $(function () {
      * */
     var addStoreToList = function (store) {
         $('#selectStore').append('<option value="' + store + '">' + store.toUpperCase() + '</option>');
+    };
+
+    /**
+     * Edit gadget
+     * @param type {String}
+     * @param id {String}
+     * @param store {String}
+     * */
+    var editAsset =  function (type,id, store) {
+        var addParam = '?id=' + id +'&type='+ type+ '&store='+ store + '&editable=true';
+        url =editGadgetUrl+ addParam;
+        window.open(url);
     };
 
     /**
@@ -219,7 +234,7 @@ $(function () {
      */
     var manipulateGadgetUsageInfo = function (usage) {
         var message = 'This gadget is used in ';
-        var endMessage = " dashboard(s). Deleting this gadget will affect the functionality of those dashboard(s)";
+        var endMessage = " dashboard(s). This action on this gadget will affect the functionality of those dashboard(s)";
         var count = 0;
         for (var index = 0; index < usage.length && count < 2; index++) {
             if (usage[index].indexOf("$") > -1) {
@@ -313,8 +328,43 @@ $(function () {
         portal.on('click', '.ds-assets .ds-asset-download-handle', function (e) {
             var assetElement = $(this).closest('.ds-asset');
             var id = assetElement.data('id');
-            var store = $('#selectStore').selectpicker('val');
+            var store = document.getElementById('store').innerHTML;
             downloadAsset(assetType, id, store);
+        });
+
+        portal.on('click', '.ds-assets .ds-asset-edit-handle', function (e) {
+            var gadgetUsageInfo;
+            e.preventDefault();
+            var assetElement = $(this).closest('.ds-asset');
+            var id = assetElement.data('id');
+            var asset = findAsset(id);
+            if (assetType === 'gadget') {
+                gadgetUsageInfo = getGadgetUsageInfo(id);
+            }
+            if (!gadgetUsageInfo || gadgetUsageInfo.dashboards.length === 0) {
+                assetElement.html(editAssetConfirmHbs(asset));
+            } else {
+                gadgetUsageInfo = manipulateGadgetUsageInfo(gadgetUsageInfo.dashboards);
+                var data = {title: asset.title};
+                data.message = gadgetUsageInfo;
+                assetElement.html(editGadgetUsageConfirmHbs(data));
+            }
+        });
+
+        portal.on('click', '.ds-assets .ds-asset-edit-confirm', function (e) {
+            e.preventDefault();
+            var assetElement = $(this).closest('.ds-asset');
+            var id = assetElement.data('id');
+            var store = document.getElementById('store').innerHTML;
+            editAsset(assetType, id, store);
+        });
+
+        portal.on('click', '.ds-assets .ds-asset-edit-cancel', function (e) {
+            e.preventDefault();
+            var assetElement = $(this).closest('.ds-asset');
+            var id = assetElement.data('id');
+            var asset = findAsset(id);
+            assetElement.html(assetThumbnailHbs(asset));
         });
 
         $(window).scroll(function () {
