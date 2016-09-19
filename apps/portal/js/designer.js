5ccd96a3b029d675bba502133557dede0f348314
Megala <ktumegala@yahoo.com>
2016-09-19 17:21:32 +0530

    Fixing UES-1123

--- apps/portal/js/designer.js
+++ apps/portal/js/designer.js
@@ -2809,7 +2809,7 @@ $(function () {
                 return;
             }
             var assets = $('.ues-store-assets').find('.ues-thumbnails');
-            if (data.length > 0) {
+            if (data.length > 0 && paging.start >= 0) {
                 storeCache[type] = storeCache[type].concat(data);
             }
             paging.start += COMPONENTS_PAGE_SIZE;
