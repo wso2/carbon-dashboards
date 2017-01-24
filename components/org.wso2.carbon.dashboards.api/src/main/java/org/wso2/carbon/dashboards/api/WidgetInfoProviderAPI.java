d86c97aa94f05be86cfd34da73f35ef380fdc7d2
nisalaniroshana <nisalaniroshana@gmail.com>
2017-01-24 18:59:47 +0530

    Add APIs to retrieve widget related info and provide interface to implement for others

---
+++ components/org.wso2.carbon.dashboards.api/src/main/java/org/wso2/carbon/dashboards/api/WidgetInfoProviderAPI.java
@@ -0,0 +1,89 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.wso2.carbon.dashboards.api;
+
+import org.wso2.carbon.dashboards.core.widget.info.WidgetDataHolder;
+import org.wso2.carbon.dashboards.core.widget.info.WidgetInfoProvider;
+import org.wso2.msf4j.Microservice;
+
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/**
+ * This is the back-end api to get the widget related information.
+ */
+public class WidgetInfoProviderAPI implements Microservice {
+
+    /**
+     * This method returns a list of meta information of widgets.
+     * @return List of widget related meta information
+     */
+    @GET
+    @Path("/metainfo")
+    @Produces("application/json")
+    public List getWidgetsMetaInfo() {
+        List<WidgetInfoProvider> widgetInfoProviderImplList = WidgetDataHolder.getInstance().getWidgetInfoProviders();
+        List list = widgetInfoProviderImplList.stream().map(widget -> widget.getWidgetsMetaInfo())
+                .filter(Objects::nonNull).collect(Collectors.toList());
+        return list;
+    }
+
+    /**
+     * This method return the configuration of given widget.
+     * @param widgetId widget id
+     * @return  widget configuration
+     */
+    @GET
+    @Path("/conf/{id}")
+    @Produces("application/json")
+    public String getWidgetConf(@PathParam("id") String widgetId) {
+        List<WidgetInfoProvider> widgetInfoProviderImplList = WidgetDataHolder.getInstance().getWidgetInfoProviders();
+        String widgetConf = widgetInfoProviderImplList.stream()
+                .map(widget -> widget.getWidgetConf(widgetId).orElse(null)).filter(Objects::nonNull).findFirst()
+                .orElse(null);
+        widgetConf = (widgetConf != null) ? widgetConf : "{\"error\":\"Error in retrieving widget configuration\"}";
+        return widgetConf;
+    }
+
+    /**
+     * This method provides thumbnail of given widget.
+     * @param widgetId widget id
+     * @return thumbnail of given widget
+     */
+    @GET
+    @Path("/thumbnail/{id}")
+    public Response getThumbnail(@PathParam("id") String widgetId) {
+        List<WidgetInfoProvider> widgetInfoProviderImplList = WidgetDataHolder.getInstance().getWidgetInfoProviders();
+        java.nio.file.Path path = widgetInfoProviderImplList.stream()
+                .map(widget -> widget.getThumbnail(widgetId).orElse(null)).filter(Objects::nonNull).findFirst()
+                .orElse(null);
+        if (path != null && Files.exists(path)) {
+            return Response.ok(path.toFile()).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).entity("Error in retrieving widget thumbnail").build();
+    }
+}
