1719e73132ab9cfe23d8da516f8420c98ffd85c9
lakshman <nyludaya@gmail.com>
2017-03-24 17:41:46 +0530

    Added support for PostgreSQL, DB2, Oracle and MSSQL databases for gadget creation.

--- apps/portal/extensions/providers/rdbms/api.js
+++ apps/portal/extensions/providers/rdbms/api.js
@@ -45,7 +45,16 @@ var getConfig, validate, getMode, getSchema, getData, registerCallBackforPush;
         "long" : NUMBER,
         "double" : NUMBER,
         "float" : NUMBER,
-        "timestamp" : NUMBER
+        "timestamp" : NUMBER,
+        "varchar2" : STRING,
+        "number" : NUMBER,
+        "datetime" : STRING,
+        "character varying" : STRING,
+        "bigserial" : NUMBER,
+        "nvarchar2" : STRING,
+        "datetime2" : STRING,
+        "nvarchar" : STRING,
+        "ntext" : STRING
     };
 
 
@@ -61,7 +70,8 @@ var getConfig, validate, getMode, getSchema, getData, registerCallBackforPush;
     validate = function (providerConfig) {
         var db = null;
         try {
-            db = new Database(providerConfig['db_url'], providerConfig['username'], providerConfig['password']);
+            db = new Database(providerConfig['dbUrl'], providerConfig['userName'], providerConfig['password'],
+             {driverClassName : providerConfig['driverClassName']});
         } catch (e) {
             return {
                 "error" : true,
@@ -89,23 +99,28 @@ var getConfig, validate, getMode, getSchema, getData, registerCallBackforPush;
     getSchema = function (providerConfig) {
         var db = null;
         try {
-            var databaseUrl = providerConfig['db_url'];
+            var databaseUrl = providerConfig['dbUrl'];
             var databaseName = databaseUrl.substring(databaseUrl.lastIndexOf("/") + 1);
-            var tableName = providerConfig['table_name'];
+            var tableName = providerConfig['tableName'];
             var db_query = '';
             var COLUMN_NAME = 'column_name';
             var COLUMN_TYPE = 'column_type';
-            var isH2 = providerConfig['db_url'].toLowerCase().indexOf('h2') > 0 ;
-            if (isH2) {
-                db_query = "SELECT column_name, type_name from information_schema.columns where table_name='" + tableName + "';";
-                COLUMN_NAME = 'COLUMN_NAME';
-                COLUMN_TYPE = 'TYPE_NAME';
+            var databases = getConfig().databases;
+            for (var i in databases) {
+                if (databaseUrl.toLowerCase().indexOf(databases[i].databaseType) > 0) {
+                    db_query = databases[i].queries.getSchema + tableName;
+                    if (databases[i].queries.appendTableSchema) {
+                        db_query += databases[i].queries.appendTableSchema + databaseName;
+                    }
+                    if(databases[i].queries.statementEnd) {
+                        db_query += databases[i].queries.statementEnd;
+                    }
+                    COLUMN_NAME = databases[i].ColumnName;
+                    COLUMN_TYPE = databases[i].ColumnType;
+                }
             }
-            else {
-                db_query = "SELECT column_name, column_type FROM INFORMATION_SCHEMA.columns where table_schema='" +
-                    databaseName + "' and table_name='" + tableName + "';";
-            }
-            db = new Database(providerConfig['db_url'], providerConfig['username'], providerConfig['password']);
+            db = new Database(providerConfig['dbUrl'], providerConfig['userName'], providerConfig['password'],
+                              {driverClassName : providerConfig['driverClassName']});
             var schema = db.query(db_query);
             var selectQuerySchema = getSelectQueryFields(providerConfig);
             if (schema.length != 0) {
@@ -165,11 +180,27 @@ var getConfig, validate, getMode, getSchema, getData, registerCallBackforPush;
     getData = function (providerConfig, limit) {
         var data;
         var db = null;
+        var databaseUrl = providerConfig['dbUrl'];
         try {
-            db = new Database(providerConfig['db_url'], providerConfig['username'], providerConfig['password']);
+            db = new Database(providerConfig['dbUrl'], providerConfig['userName'], providerConfig['password'],
+                              {driverClassName : providerConfig['driverClassName']});
             var query = providerConfig['query'];
             if (limit) {
-                query = query.replace(/^\s\s*/, '').replace(/\s\s*$/, '') + ' limit ' + limit;
+                query = query.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+                var databases = getConfig().databases;
+                for (var i in databases) {
+                    if (databaseUrl.toLowerCase().indexOf(databases[i].databaseType) > 0) {
+                        var limitQuery = databases[i].queries.limit;
+                        query += limitQuery ? limitQuery : "";
+                        var topQuery = databases[i].queries.top;
+                        if (topQuery) {
+                            var indexOfSelect = query.toLowerCase().indexOf("select");
+                            query = query.slice(0, indexOfSelect + 6) + topQuery + query.slice(indexOfSelect + 6);
+                            
+                        }
+                        query = query.replace(/\?/g, limit);
+                    }
+                }
             }
             data = db.query(query);
         }
