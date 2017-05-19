cd6f10341a949d3c121290a980b77a053a978fd7
Udara Rathnayake <udarakr@gmail.com>
2017-05-19 12:51:24 +0530

    Update sample pub/sub widgets to render multiple instances

--- components/org.wso2.carbon.dashboards.designer/src/main/fragments/publisher/public/js/publisher.js
+++ components/org.wso2.carbon.dashboards.designer/src/main/fragments/publisher/public/js/publisher.js
@@ -1,6 +1,13 @@
 (function () {
     "use strict";
-    $("#sendMsg").click(function(){
-        pubsub.publish($('#msg').val(), 'PUBLISHER');
-    }); 
+    var bind = function (containerId) {
+	    $("#" + containerId + " .sendMsg").click(function() {
+	       var instanceID = $(this).closest(".dashboard-component-box").attr("id");
+	        portal.dashboards.functions.designer.publishToTopics($("#" + containerId + " .msg").val(), instanceID);
+	    });
+    }
+
+    portal.dashboards.widgets['org.wso2.carbon.dashboards.designer.publisher'].actions = {
+        bind:bind
+    }
 }());
