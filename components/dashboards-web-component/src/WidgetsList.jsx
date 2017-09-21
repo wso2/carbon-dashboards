079734c94ab98b309800def0ba2d08e3f1178fa5
NisalaNiroshana <nisalaniroshana@gmail.com>
2017-09-21 09:20:13 +0530

    Revert "Add multiple page support functions"
    This reverts commit 9d4860b6d7e0426d9b79384df87728e6aca81c2f.

--- components/dashboards-web-component/src/WidgetsList.jsx
+++ components/dashboards-web-component/src/WidgetsList.jsx
@@ -21,126 +21,69 @@ import React from 'react';
 import Drawer from 'material-ui/Drawer';
 import TextField from 'material-ui/TextField';
 import Divider from 'material-ui/Divider';
-import WidgetInfoAPIS from './utils/widget-info-apis';
-import {widgetLoadingComponent, dashboardLayout} from './WidgetLoadingComponent';
+
 import WidgetListThumbnail from './WidgetListThumbnail';
-
-const styles = {
-    open: {
-    },
-    close: {
-        display: 'none'
-    }
-};
-
-var widgets = [];
-var isDashboardLoaded = false;
 
 class WidgetsList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            widgets: [],
-            show: this.props.show
-        }
-
         this.searchWidget = this.searchWidget.bind(this);
-        // this.isDisplayed = this.isDisplayed.bind(this);
-        this.setWidgets = this.setWidgets.bind(this);
-    }
+        this.state = {
+            widgetList: this.props.widgetList,
+            filteredWidgetSet: new Set(this.props.widgetList)
+        };
+        this.isDisplayed = this.isDisplayed.bind(this);
 
-    // searchWidget(event, value) {
-    //     let filteredWidgetList = this.props.widgetList.filter(function (widget) {
-    //         return widget.name.toLowerCase().includes(value.toLowerCase());
-    //     });
-    //     this.state.filteredWidgetSet = new Set(filteredWidgetList.map((widget) => {
-    //         return widget.name
-    //     }));
-    //     this.setState({widgetList: filteredWidgetList});
-    // }
+    }
 
     searchWidget(event, value) {
-        let filteredWidgetList = widgets.filter(function (widget) {
+        let filteredWidgetList = this.props.widgetList.filter(function (widget) {
             return widget.name.toLowerCase().includes(value.toLowerCase());
         });
-        this.setState({widgets: filteredWidgetList});
+        this.state.filteredWidgetSet = new Set(filteredWidgetList.map((widget) => {
+            return widget.name
+        }));
+        this.setState({widgetList: filteredWidgetList});
     }
 
-    // componentWillReceiveProps(props) {
-    //     this.state = {
-    //         widgetList: this.props.widgetList,
-    //         filteredWidgetSet: new Set(props.widgetList.map((widget) => {
-    //             return widget.name
-    //         }))
-    //     }
-    // }
-
-    // isDisplayed(widgetId) {
-    //     if (this.state.filteredWidgetSet.has(widgetId)) {
-    //         return "block";
-    //     } else {
-    //         return "none";
-    //     }
-    // }
-
-    componentDidMount() {
-        let widgetInfoAPIS = new WidgetInfoAPIS();
-        let promisedWidgetInfo = widgetInfoAPIS.getWidgetsInfo();
-        promisedWidgetInfo
-            .then(this.setWidgets)
-            .catch(function (error) {
-            //TODO Need to use proper notification library to show the error
-        });
-        widgetLoadingComponent.setfinishedRegisteringCallback(this.initializeWidgetList);
-        widgetLoadingComponent.onStateChanged(function(config) {
-            console.log('>>>>>>>>>>>>');
-            console.log(config);
-        })
-    }
-
-    setWidgets(response) {
-        widgets = response.data;
-        this.setState({
-            widgets: widgets
-        }, this.initializeWidgetList);
-    }
-
-    initializeWidgetList(isWidgetsLoaded, initDashboardFlag) {
-        let newItemConfig;
-        if (isWidgetsLoaded) {
-            isDashboardLoaded = true;
+    componentWillReceiveProps(props) {
+        this.state = {
+            widgetList: this.props.widgetList,
+            filteredWidgetSet: new Set(props.widgetList.map((widget) => {
+                return widget.name
+            }))
         }
-        if (!(widgets.length === 0) && isDashboardLoaded) {
-            widgets.map(widget => {
-                newItemConfig = {
-                    title: widget.name,
-                    type: 'react-component',
-                    component: widget.name
-                };
-                widgetLoadingComponent.createDragSource(document.getElementById(widget.name), newItemConfig);
-                widgetLoadingComponent.loadWidget(widget.name);
-            });
-            if (initDashboardFlag) {
-                widgetLoadingComponent.initializeDashboard();
-            }
+    }
+
+    isDisplayed(widgetId) {
+        if (this.state.filteredWidgetSet.has(widgetId)) {
+            return "block";
+        } else {
+            return "none";
         }
     }
 
     render() {
-        return (
-            <div style={this.props.show ? styles.open : styles.close}>
+        return (<div>
+            <Drawer width={250} open={this.props.widgetListToggled} containerClassName="designer-widget-list-panel">
                 <div className="widget-list-header">WIDGETS LIST</div>
                 <div className="widget-search-div">
-                    <TextField onChange={this.searchWidget} hintText="Search.." />
+                    <TextField onChange={this.searchWidget} hintText="Search.."
+                               className="widget-search-textfield"></TextField>
                 </div>
                 <Divider/>
                 {
-                    this.state.widgets.map(widget => {
-                        return <WidgetListThumbnail widgetID={widget.name} isDisplayed="block" widgetName={widget.name} />;
+                    this.props.widgetList.map(widget => {
+                        return <WidgetListThumbnail data={this.state.widgetList} widgetID={widget.name}
+                                                    isDisplayed={this.isDisplayed(widget.name)}
+                                                    widgetName={widget.name}></WidgetListThumbnail>;
                     })
                 }
+            </Drawer>
         </div>);
     }
+
+
 }
 
 export default WidgetsList;
