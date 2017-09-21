d47fa72fecd6ea476ef09df2e3395c69964e8ac7
nisalaniroshana <nisalaniroshana@gmail.com>
2017-09-21 07:34:05 +0530

    Add dashboard settings page

---
+++ components/dashboards-web-component/src/DashboardSettings.jsx
@@ -0,0 +1,191 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
+import Snackbar from 'material-ui/Snackbar';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import Clear from 'material-ui/svg-icons/content/clear';
+
+import Header from './Header';
+import DashboardAPIS from './utils/dashboard-apis';
+import DashboardUtils from './utils/dashboard-utils';
+
+const muiTheme = getMuiTheme(darkBaseTheme);
+const hintStyle = {color: "grey", bottom: 0};
+const textareaStyle = {color: "black"};
+const underlineStyle = {bottom: 0, bottomBorder: "1px solid #424242", color: "blue"};
+const errorFieldStyle = {color: "#FF5722", marginTop: 5};
+const textFieldStyle = {marginLeft: 15, width: 500, height: 25, color: "black"};
+const messageBoxStyle = {textAlign: "center", color: "white"};
+const errorMessageStyle = {backgroundColor: "#FF5722", color: "white"};
+const successMessageStyle = {backgroundColor: "#4CAF50", color: "white"};
+
+class DashboardSettings extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            showMsg: false,
+            errorMessageName: "",
+            errorStyleName: "",
+            errorMessageURL: "",
+            errorStyleURL: "",
+            dashboardName: "",
+            dashboardDescription: "",
+            Message: ""
+
+        };
+        this.handleDashboardName = this.handleDashboardName.bind(this);
+        this.handleDashboardDescription = this.handleDashboardDescription.bind(this);
+        this.handleRequestClose = this.handleRequestClose.bind(this);
+        this.updateDashboard = this.updateDashboard.bind(this);
+        this.showMessage = this.showMessage.bind(this);
+        this.showError = this.showError.bind(this);
+    }
+
+    handleDashboardName(event, value) {
+        this.setState({
+            dashboardName: value,
+            errorMessageName: "",
+            errorStyleName: ""
+        });
+    }
+
+    handleDashboardDescription(event, value) {
+        this.setState({
+            dashboardDescription: value,
+        });
+    }
+
+    handleRequestClose() {
+        this.setState({
+            showMsg: false,
+            Message: ""
+        });
+    }
+
+    componentWillMount() {
+        let dashboardAPIs = new DashboardAPIS();
+        let promised_dashboard = dashboardAPIs.getDashboardByID(this.props.match.params.id);
+        let that = this;
+        promised_dashboard.then(function (response) {
+            that.setState({
+                dashboardName: response.data.name,
+                dashboardDescription: response.data.description,
+                dashboard: response.data
+            });
+        })
+    }
+
+    updateDashboard() {
+        let dashboardAPIs = new DashboardAPIS();
+        this.state.dashboard.name = this.state.dashboardName;
+        this.state.dashboard.description = this.state.dashboardDescription;
+        this.state.dashboard.pages = JSON.parse(this.state.dashboard.pages);
+        if (this.state.dashboardName.length === 0) {
+            this.showError("Please fill the dashboard name field !!");
+            return;
+        }
+        let that = this;
+        dashboardAPIs.updateDashboardByID(this.props.match.params.id, this.state.dashboard).then(function (response) {
+            if (response.status === 200) {
+                that.showMessage("Dashboard '" + that.state.dashboard.name + "' is updated successfully !!");
+            }
+        })
+    }
+
+    showError(message) {
+        this.setState({
+            messageStyle: errorMessageStyle,
+            showMsg: true,
+            Message: message
+        });
+    }
+
+    showMessage(message) {
+        this.setState({
+            messageStyle: successMessageStyle,
+            showMsg: true,
+            Message: message
+        });
+    }
+
+    render() {
+        return <MuiThemeProvider muiTheme={muiTheme}>
+            <div>
+                <Header dashboardName="Portal"></Header>
+                <div className="portal-navigation-bar">
+                    <Link to={"/portal/"}>
+                        <FloatingActionButton mini={true} className="navigation-icon">
+                            <Clear />
+                        </FloatingActionButton>
+                        <div className="dashboard-settings-text"> Cancel
+                        </div>
+                    </Link>
+                </div>
+                <div className="dashboard-settings-page-form-container">
+                    <h1 className="dashboard-settings-header">Dashboard Settings</h1>
+                    <Divider className="dashboard-settings-divider"/>
+                    <div className="dashboard-settings-form-group">
+                        <label className="dashboard-settings-page-label-url">
+                            URL
+                        </label>
+                        <div className="dashboard-settings-url">
+                            {"/" + this.props.match.params.id}
+                        </div>
+                    </div>
+                    <div className="dashboard-settings-form-group">
+                        <label className="dashboard-settings-page-label-name">
+                            Name of your Dashboard
+                        </label>
+                        <TextField errorText={this.state.errorMessageName} errorStyle={this.state.errorStyleName}
+                                   onChange={this.handleDashboardName} id="dashboardName" hintStyle={hintStyle}
+                                   textareaStyle={textareaStyle}
+                                   underlineStyle={underlineStyle} style={textFieldStyle}
+                                   hintText="E.g. Sales Statistics" value={this.state.dashboardName}/>
+                    </div>
+                    <div className="dashboard-settings-form-group">
+                        <label className="dashboard-settings-page-label-description">
+                            Description
+                        </label>
+                        <TextField id="dashboardDescription" hintStyle={hintStyle} textareaStyle={textareaStyle}
+                                   underlineStyle={underlineStyle} style={textFieldStyle}
+                                   onChange={this.handleDashboardDescription} hintText="E.g. Monthly Sales Statistics"
+                                   value={this.state.dashboardDescription}/>
+                    </div>
+                    <RaisedButton onClick={this.updateDashboard} label="Save" primary={true}
+                                  className="dashboard-settings-save-button"/>
+                </div>
+                <Snackbar contentStyle={messageBoxStyle} bodyStyle={this.state.messageStyle} open={this.state.showMsg}
+                          message={this.state.Message} autoHideDuration={4000}
+                          onRequestClose={this.handleRequestClose}/>
+            </div>
+        </MuiThemeProvider>;
+    }
+}
+
+export default DashboardSettings;
--- components/dashboards-web-component/src/DashboardThumbnail.jsx
+++ components/dashboards-web-component/src/DashboardThumbnail.jsx
@@ -20,7 +20,38 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import DashboardAPIs from './utils/dashboard-apis';
+
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+
 class DashboardThumbnail extends React.Component {
+
+    constructor() {
+        super();
+        this.deleteDashboard = this.deleteDashboard.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleOpen = this.handleOpen.bind(this);
+        this.state = {
+            deleteDashboardDialog: false,
+            thumbnailStyle: ""
+        };
+    }
+
+    handleOpen() {
+        this.setState({deleteDashboardDialog: true});
+    }
+
+    handleClose() {
+        this.setState({deleteDashboardDialog: false});
+    }
+
+    deleteDashboard() {
+        let dashboardAPIs = new DashboardAPIs();
+        dashboardAPIs.deleteDashboardByID(this.props.dashboard.url);
+        this.setState({deleteDashboardDialog: false, thumbnailStyle: "dashboard-thumbnail-deleted"});
+    }
+
     render() {
         let styles = {
             card: {
@@ -30,37 +61,64 @@ class DashboardThumbnail extends React.Component {
                 "color": this.props.muiTheme.palette.textColor
             },
             subtitle: {
-                "color": this.props.muiTheme.palette.alternateTextColor
+                "color": this.props.muiTheme.palette.alternateTextColor,
+                height: 17
             }
         };
 
+        let actionsButtons = [
+            <FlatButton
+                label="Yes"
+                primary={true}
+                onClick={this.deleteDashboard}
+            />,
+            <FlatButton
+                label="No"
+                primary={true}
+                onClick={this.handleClose}
+            />,
+        ];
+
         return (
-            <div className="dashboard-thumbnail">
-                <div style={styles.card} className="content">
-                    <h4 style={styles.title}>{this.props.dashboard.name}</h4>
-                    <p style={styles.subtitle}>{this.props.dashboard.description}</p>
-                    <div className="actions">
-                        <Link to={"dashboards/" + this.props.dashboard.url}>
+            <div className={this.state.thumbnailStyle}>
+                <Dialog
+                    title={"Do you want to delete '" + this.props.dashboard.name + "' ?"}
+                    actions={actionsButtons}
+                    modal={true}
+                    open={this.state.deleteDashboardDialog}
+                ></Dialog>
+                <div className="dashboard-thumbnail">
+                    <div className="trash-button-div" onClick={this.handleOpen}>
+                        <a href="#" className="trash-button-icon">
+                            <i className="fw fw-delete fw-stack-1x" title="Delete Dashboard"></i>
+                        </a>
+                    </div>
+                    <div style={styles.card} className="content">
+                        <h4 style={styles.title}>{this.props.dashboard.name}</h4>
+                        <p style={styles.subtitle}>{this.props.dashboard.description}</p>
+                        <div className="actions">
+                            <Link to={"dashboards/" + this.props.dashboard.url}>
                             <span className="fw-stack icon">
                                 <i className="fw fw-circle-outline fw-stack-2x"></i>
                                 <i className="fw fw-view fw-stack-1x"></i>
                             </span>
-                            View
-                        </Link>
-                        <Link to={"designer/" + this.props.dashboard.url}>
-                            <span className="fw-stack icon">
+                                View
+                            </Link>
+                            <Link to={"designer/" + this.props.dashboard.url}>
+                            <span className="fw-stack icon edit-dashboard-icon">
                                 <i className="fw fw-circle-outline fw-stack-2x"></i>
                                 <i className="fw fw-edit fw-stack-1x"></i>
                             </span>
-                            Design
-                        </Link>
-                        <a href="" className="disabled">
-                            <span className="fw-stack icon">
+                                Design
+                            </Link>
+                            <Link to={"settings/" + this.props.dashboard.url}>
+                            <span className="fw-stack icon edit-dashboard-icon">
                                 <i className="fw fw-circle-outline fw-stack-2x"></i>
-                                <i className="fw fw-settings fw-stack-1x"></i>
+                                <i className="fw fw-edit fw-stack-1x"></i>
                             </span>
-                            Settings
-                        </a>
+                                Settings
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
