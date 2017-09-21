6f0e12c057261b3ea8c481fbed45a6d28feb0778
nisalaniroshana <nisalaniroshana@gmail.com>
2017-09-21 12:03:18 +0530

    Fix rendering issue in dashboard designer

--- components/dashboards-web-component/src/designer/components/PagesPanel.jsx
+++ components/dashboards-web-component/src/designer/components/PagesPanel.jsx
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import AddCircleOutlineIcon from 'material-ui/svg-icons/content/add-circle-outline';
@@ -55,13 +74,15 @@ export default class PagesPanel extends Component {
         return (
             <div style={this.props.show ? styles.open : styles.close}>
                 <div style={{'text-align': 'center'}}>
-                    <RaisedButton label="Create Page" primary={true} icon={<AddCircleOutlineIcon />} style={{margin: 12}} onClick={this.addPage} />
+                    <RaisedButton label="Create Page" primary={true} icon={<AddCircleOutlineIcon />}
+                                  style={{margin: 12}} onClick={this.addPage} />
                     <TextField hintText="Search..." onChange={this.searchPages} />
                 </div>
                 {
                     this.state.pages.map(p => {
                         return (
-                            <PageEntry page={p} dashboardUrl={this.props.dashboard.url} pageUrl={p.url} onPageChanged={this.savePage} />
+                            <PageEntry page={p} dashboardUrl={this.props.dashboard.url} pageUrl={p.url}
+                                       onPageChanged={this.savePage} />
                         );
                     })
                 }
