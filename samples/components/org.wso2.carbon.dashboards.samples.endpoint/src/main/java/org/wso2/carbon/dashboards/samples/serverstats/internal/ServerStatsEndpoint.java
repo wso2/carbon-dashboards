714d0e8417675dd1b0931aa4e390ff147f10a507
Sajith Janaprasad <sajithar@wso2.com>
2017-11-02 05:24:47 +0530

    fix ServerStatsEndpoint class in stat enpoint sample component

--- samples/components/org.wso2.carbon.dashboards.samples.endpoint/src/main/java/org/wso2/carbon/dashboards/samples/serverstats/internal/ServerStatsEndpoint.java
+++ samples/components/org.wso2.carbon.dashboards.samples.endpoint/src/main/java/org/wso2/carbon/dashboards/samples/serverstats/internal/ServerStatsEndpoint.java
@@ -18,19 +18,27 @@
 
 package org.wso2.carbon.dashboards.samples.serverstats.internal;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.Deactivate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.wso2.carbon.dashboards.samples.serverstats.ServerStatsProvider;
-import org.wso2.carbon.dashboards.samples.serverstats.StatsConsumer;
+import org.wso2.carbon.dashboards.samples.serverstats.api.StatType;
+import org.wso2.carbon.dashboards.samples.serverstats.internal.cpu.CpuStatsProvider;
+import org.wso2.carbon.dashboards.samples.serverstats.internal.memory.MemoryStatsProvider;
+import org.wso2.carbon.dashboards.samples.serverstats.spi.ServerStatsProvider;
+import org.wso2.carbon.dashboards.samples.serverstats.spi.Stat;
 import org.wso2.msf4j.websocket.WebSocketEndpoint;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.EnumMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -53,39 +61,42 @@ public class ServerStatsEndpoint implements WebSocketEndpoint {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ServerStatsEndpoint.class);
 
-    private final Map<String, String> statsConsumerRegistrations = new HashMap<>();
-    private ServerStatsProvider statsProvider;
+    private final Map<StatType, ServerStatsProvider> serverStatsProviders = new EnumMap<>(StatType.class);
+    private final Map<Session, StatType> serverStatsConsumers = new ConcurrentHashMap<>();
 
-    @Reference(service = ServerStatsProvider.class,
-               cardinality = ReferenceCardinality.MANDATORY,
-               policy = ReferencePolicy.DYNAMIC,
-               unbind = "unbindServerStatsProvider")
-    protected void setServerStatsProvider(ServerStatsProvider statsProvider) {
-        this.statsProvider = statsProvider;
-        LOGGER.debug("An instance of '{}' registered as a {}.", statsProvider.getClass(),
-                     ServerStatsProvider.class.getCanonicalName());
+    private final ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(5);
+
+    public ServerStatsEndpoint() {
+        serverStatsProviders.put(StatType.CPU, new CpuStatsProvider());
+        serverStatsProviders.put(StatType.MEMORY, new MemoryStatsProvider());
     }
 
-    protected void unbindServerStatsProvider(ServerStatsProvider statsProvider) {
-        this.statsProvider = null;
-        LOGGER.debug("An instance of '{}' unregistered as a {}.", statsProvider.getClass(),
-                     ServerStatsProvider.class.getCanonicalName());
+    @Activate
+    protected void activate(BundleContext bundleContext) {
+        start();
+        LOGGER.debug("{} activated.", this.getClass().getName());
+    }
+
+    @Deactivate
+    protected void deactivate(BundleContext bundleContext) {
+        stop();
+        LOGGER.debug("{} deactivated.", this.getClass().getName());
     }
 
     @OnOpen
     public void onOpen(@PathParam("type") String type, Session session) {
-        statsConsumerRegistrations.put(session.getId(),
-                                       statsProvider.registerConsumer(new EndpointStatsConsumer(type, session)));
+        serverStatsConsumers.put(session, StatType.valueOf(type.toUpperCase(Locale.US)));
     }
 
     @OnMessage
     public String onTextMessage(@PathParam("type") String type, String text, Session session) {
-        return String.valueOf(statsProvider.getStats(type));
+        ServerStatsProvider statsProvider = serverStatsProviders.get(StatType.valueOf(type.toUpperCase(Locale.US)));
+        return statsProvider.getStat().toJson();
     }
 
     @OnClose
-    public void onClose(@PathParam("type") String name, CloseReason closeReason, Session session) {
-        statsProvider.unregisterConsumer(statsConsumerRegistrations.get(session.getId()));
+    public void onClose(@PathParam("type") String type, CloseReason closeReason, Session session) {
+        serverStatsConsumers.remove(session);
     }
 
     @OnError
@@ -93,29 +104,24 @@ public class ServerStatsEndpoint implements WebSocketEndpoint {
         LOGGER.error("An error occurred for '{}'.", session.getId(), throwable);
     }
 
-    private static class EndpointStatsConsumer implements StatsConsumer {
+    private void start() {
+        scheduledExecutor.scheduleWithFixedDelay(this::publishStatsToConsumers, 0, 1, TimeUnit.SECONDS);
+    }
 
-        private static final Logger LOGGER = LoggerFactory.getLogger(EndpointStatsConsumer.class);
+    private void stop() {
+        scheduledExecutor.shutdown();
+    }
 
-        private final String type;
-        private final Session session;
-
-        public EndpointStatsConsumer(String type, Session session) {
-            this.type = type;
-            this.session = session;
-        }
-
-        @Override
-        public String getType() {
-            return type;
-        }
-
-        @Override
-        public void consume(double value) {
-            try {
-                session.getBasicRemote().sendText(String.valueOf(value));
-            } catch (IOException e) {
-                LOGGER.error("Cannot send stats to '{}'.", session.getId(), e);
+    private void publishStatsToConsumers() {
+        for (Map.Entry<Session, StatType> entry : serverStatsConsumers.entrySet()) {
+            ServerStatsProvider statsProvider = serverStatsProviders.get(entry.getValue());
+            if (statsProvider != null) {
+                Stat stat = statsProvider.getStat();
+                try {
+                    entry.getKey().getBasicRemote().sendText(stat.toJson());
+                } catch (IOException e) {
+                    LOGGER.error("Cannot send stats to '{}'.", entry.getKey().getId(), e);
+                }
             }
         }
     }
