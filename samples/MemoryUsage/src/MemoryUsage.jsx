024c278817b070aa5b51446ad5446aa3bd37e54d
Sajith Janaprasad <sajithar@wso2.com>
2017-11-01 09:08:06 +0530

    add websocket support to MemoryUsage sample widget

--- samples/MemoryUsage/src/MemoryUsage.jsx
+++ samples/MemoryUsage/src/MemoryUsage.jsx
@@ -1,24 +1,23 @@
 /*
- *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
-import React, { Component } from 'react';
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import React, {Component} from 'react';
+import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from 'recharts';
 
 var data = [];
 
@@ -26,6 +25,7 @@ const DATA_POINT_COUNT = 25;
 const REFRESH_RATE = 1;
 
 class MemoryUsage extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -36,8 +36,13 @@ class MemoryUsage extends Component {
         };
         this.handleResize = this.handleResize.bind(this);
         this.props.glContainer.on('resize', this.handleResize);
+
         this.publishData = this.publishData.bind(this);
-        this.publishData();
+        const self = this;
+        const ws = new WebSocket('ws://localhost:8080/server-stats/memory');
+        ws.onmessage = function(event) {
+            self.publishData(event.data);
+        };
     }
 
     formatDateLabel(dt) {
@@ -47,15 +52,15 @@ class MemoryUsage extends Component {
         return (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
     }
 
-    publishData() {
+    publishData(memoryUsage) {
         data.push({
-            timestamp: this.formatDateLabel(new Date()), 
-            memory: Math.round(Math.random() * 100)
+            timestamp: this.formatDateLabel(new Date()),
+            memory: memoryUsage * 100
         });
 
         let arr = [];
         let count = data.length > DATA_POINT_COUNT ? data.length : DATA_POINT_COUNT;
-        for(var i = 0; i < count; i++) {
+        for (var i = 0; i < count; i++) {
             arr.push(data[i] || {timestamp: '', memory: null});
         }
 
@@ -66,33 +71,33 @@ class MemoryUsage extends Component {
         this.setState({
             data: arr
         });
-        setTimeout(this.publishData, Math.round(1000 / REFRESH_RATE));
+        //setTimeout(this.publishData, Math.round(1000 / REFRESH_RATE));
     }
 
-    render () {
+    render() {
         let styles = {
             font: '11px Roboto, sans-serif',
             color: '#fff'
         };
         return (
-                <section style={styles}>
-                    <LineChart width={this.state.width} height={this.state.height} data={this.state.data} 
-                        margin={{top: 30, right: 30, left: 20, bottom: 10}}>
-                        <XAxis dataKey="timestamp"/>
-                        <YAxis/>
-                        <CartesianGrid strokeDasharray="10 10" vertical={false}/>
-                        <Tooltip/>
-                        <Legend />
-                        <Line type='monotone' dataKey='memory' name='Memory %' stroke='#9C27B0'
-                             isAnimationActive={false} dot={false}/>
-                    </LineChart>
-                </section>
+            <section style={styles}>
+                <LineChart width={this.state.width} height={this.state.height} data={this.state.data}
+                           margin={{top: 30, right: 30, left: 20, bottom: 10}}>
+                    <XAxis dataKey="timestamp" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="10 10" vertical={false} />
+                    <Tooltip />
+                    <Legend />
+                    <Line type='monotone' dataKey='memory' name='Memory %' stroke='#9C27B0'
+                          isAnimationActive={false} dot={false} />
+                </LineChart>
+            </section>
         );
     }
 
     handleResize() {
         this.setState({width: this.props.glContainer.width, height: this.props.glContainer.height});
     }
-};
+}
 
 global.dashboard.registerWidget("MemoryUsage", MemoryUsage);
