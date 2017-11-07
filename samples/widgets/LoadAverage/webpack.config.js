69d8096de3e6414f25611f87d463cf201d82d1fc
Sajith Janaprasad <sajithar@wso2.com>
2017-11-07 13:09:50 +0530

    make LoadAverage to a Maven module
    org.wso2.carbon.dashboards.samples.widgets.load-average

--- samples/widgets/LoadAverage/webpack.config.js
+++ samples/widgets/LoadAverage/webpack.config.js
@@ -1,22 +1,39 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 const path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-    devtool: 'source-map',
     context: path.resolve(__dirname, './src'),
     entry: {
-        index: './LoadAverage.jsx',
+        index: './LoadAverage.jsx'
     },
     output: {
-        path: path.resolve(__dirname, './dist/'),
-        filename: 'LoadAverage.js',
+        path: path.resolve(__dirname, './dist/LoadAverage/'),
+        filename: 'LoadAverage.js'
     },
     module: {
         loaders: [
             {
                 test: /\.html$/,
-                use: [{
-                    loader: 'html-loader',
-                }],
+                use: [{loader: 'html-loader'}]
             },
             {
                 test: /\.js$/,
@@ -25,51 +42,45 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         query: {
-                            presets: ['es2015', 'react'],
-                        },
-                    },
-                ],
+                            presets: ['es2015', 'react']
+                        }
+                    }
+                ]
             },
             {
                 test: /\.(png|jpg|svg|cur|gif|eot|svg|ttf|woff|woff2)$/,
-                use: ['url-loader'],
+                use: ['url-loader']
             },
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015', 'react'],
-                },
+                    presets: ['es2015', 'react']
+                }
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader']
             },
             {
                 test: /\.scss$/,
-                use: [{
-                    loader: 'style-loader',
-                }, {
-                    loader: 'css-loader',
-                }, {
-                    loader: 'sass-loader',
-                }],
-            },
+                use: [{loader: 'style-loader'}, {loader: 'css-loader'}, {loader: 'sass-loader'}]
+            }
 
-        ],
+        ]
     },
     plugins: [
-        new webpack.ProvidePlugin({
-            'React': 'react',
-            'ReactDOM': 'react-dom'
-        })
+        new CopyWebpackPlugin([
+            {from: path.resolve(__dirname, './src/resources/')}
+        ])
     ],
     resolve: {
-        extensions: ['.js', '.json', '.jsx', '.scss'],
+        extensions: ['.js', '.json', '.jsx', '.scss']
     },
+    devtool: 'source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
-        publicPath: '/dist/',
-    },
+        publicPath: '/dist/'
+    }
 };
