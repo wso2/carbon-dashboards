84049d5212749b061f81ebd38dd12a8858f346b3
Sajith Janaprasad <sajithar@wso2.com>
2017-11-08 15:19:37 +0530

    refactor WidgetApi class

--- components/dashboards/org.wso2.carbon.dashboards.api/src/main/java/org/wso2/carbon/dashboards/api/WidgetApi.java
+++ components/dashboards/org.wso2.carbon.dashboards.api/src/main/java/org/wso2/carbon/dashboards/api/WidgetApi.java
@@ -1,24 +1,29 @@
 /*
- *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.wso2.carbon.dashboards.api;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.wso2.carbon.dashboards.core.widget.info.WidgetInfoProviderImpl;
 import org.wso2.msf4j.Microservice;
 
@@ -29,17 +34,29 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-
 /**
- * This is the back-end api to get the widget related information.
+ * REST API for widget related operations.
+ *
+ * @since 4.0.0
  */
-@Component(
-        name = "org.wso2.carbon.dashboards.api.WidgetInfoProviderAPI",
-        service = Microservice.class,
-        immediate = true
+@Component(name = "org.wso2.carbon.dashboards.api.WidgetApi",
+           service = Microservice.class,
+           immediate = true
 )
 @Path("/apis/widgets")
 public class WidgetApi implements Microservice {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WidgetApi.class);
+
+    @Activate
+    protected void activate(BundleContext bundleContext) {
+        LOGGER.debug("{} activated.", this.getClass().getName());
+    }
+
+    @Deactivate
+    protected void deactivate(BundleContext bundleContext) {
+        LOGGER.debug("{} deactivated.", this.getClass().getName());
+    }
 
     /**
      * This method returns a list of meta information of widgets.
@@ -64,6 +81,7 @@ public class WidgetApi implements Microservice {
     @Path("/{id}/conf")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getWidgetConf(@PathParam("id") String widgetId) {
+        // TODO: 11/8/17 Implement
         return Response.ok().build();
     }
 
@@ -76,6 +94,7 @@ public class WidgetApi implements Microservice {
     @GET
     @Path("/{id}/thumbnail")
     public Response getThumbnail(@PathParam("id") String widgetId) {
+        // TODO: 11/8/17 Implement
         return Response.ok().build();
     }
 }
