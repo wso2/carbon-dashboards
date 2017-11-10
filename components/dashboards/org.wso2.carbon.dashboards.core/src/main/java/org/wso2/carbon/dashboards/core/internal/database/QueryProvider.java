21ee0620b5e07a6e7d34aa69d9c4745e5959775f
Sajith Janaprasad <sajithar@wso2.com>
2017-11-10 17:28:08 +0530

    update QueryProvider class to load default SQl queries

--- components/dashboards/org.wso2.carbon.dashboards.core/src/main/java/org/wso2/carbon/dashboards/core/internal/database/QueryProvider.java
+++ components/dashboards/org.wso2.carbon.dashboards.core/src/main/java/org/wso2/carbon/dashboards/core/internal/database/QueryProvider.java
@@ -19,51 +19,60 @@ package org.wso2.carbon.dashboards.core.internal.database;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.wso2.carbon.config.provider.ConfigProvider;
 import org.wso2.carbon.dashboards.core.bean.DashboardConfigurations;
+import org.wso2.carbon.dashboards.core.exception.DashboardRuntimeException;
 
-import java.util.HashMap;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
+import java.util.Properties;
 
 /**
- * Holds the database queries.
+ * Provides SQl queries.
+ *
+ * @since 4.0.0
  */
 public class QueryProvider {
+
+    // TODO: 11/10/17 Re-think this class after the common SQL query provider get released.
 
     public static final String ADD_DASHBOARD_CONTENT_QUERY = "add_dashboard";
     public static final String GET_DASHBOARD_METADATA_LIST_QUERY = "get_dashboard_metadata_list";
     public static final String GET_DASHBOARD_BY_URL_QUERY = "get_dashboard_by_url";
     public static final String DELETE_DASHBOARD_BY_URL_QUERY = "delete_dashboard_by_url";
     public static final String UPDATE_DASHBOARD_CONTENT_QUERY = "update_dashboard_content";
+    private static final String FILE_DEFAULT_SQL_QUERIES = "default-sql-queries.properties";
     private static final Logger LOGGER = LoggerFactory.getLogger(QueryProvider.class);
 
     private final Map<String, String> queries;
 
-    public QueryProvider(ConfigProvider configProvider) {
-        this.queries = readConfigs(configProvider);
+    public QueryProvider(DashboardConfigurations dashboardConfigurations) {
+        this.queries = readConfigs(dashboardConfigurations);
     }
 
-    private Map<String, String> readConfigs(ConfigProvider configProvider) {
-        // TODO: Get the relevant type of the database from the config provider.
-        String databaseType = "h2";
-        try {
-            DashboardConfigurations dashboardConfigurations = configProvider.getConfigurationObject(
-                    DashboardConfigurations.class);
-            if (!dashboardConfigurations.getQueries().containsKey(databaseType)) {
-                throw new RuntimeException("Unable to find the database type: " + databaseType);
+    @SuppressWarnings("unchecked, rawtypes")
+    private Map<String, String> readConfigs(DashboardConfigurations dashboardConfigurations) {
+        Properties defaultSqlQueries = new Properties();
+        try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(FILE_DEFAULT_SQL_QUERIES)) {
+            if (inputStream == null) {
+                throw new DashboardRuntimeException(
+                        "Cannot find file '" + FILE_DEFAULT_SQL_QUERIES + "' in class path.");
             }
-            return dashboardConfigurations.getQueries().get(databaseType);
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
+            defaultSqlQueries.load(inputStream);
+        } catch (IllegalArgumentException e) {
+            throw new DashboardRuntimeException("Property file '" + FILE_DEFAULT_SQL_QUERIES + "' is invalid.", e);
+        } catch (IOException e) {
+            throw new DashboardRuntimeException("Cannot read property file '" + FILE_DEFAULT_SQL_QUERIES + "'.", e);
         }
-        return new HashMap<>();
+
+        // TODO: 11/10/17 Merge default SQL queries with DashboardConfigurations.getQueries()
+        return (Map) defaultSqlQueries;
     }
 
     public String getQuery(String key) {
         if (!this.queries.containsKey(key)) {
-            throw new RuntimeException("Unable to find the configuration entry for the key: " + key);
+            throw new DashboardRuntimeException("Cannot find the SQl query for key '" + key + "'.");
         }
         return this.queries.get(key);
     }
 }
-
