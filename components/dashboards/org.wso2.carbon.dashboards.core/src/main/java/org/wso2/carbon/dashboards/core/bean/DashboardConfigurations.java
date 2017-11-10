92ecb98dda12488205f61a4ab16cc4f8a053761a
Sajith Janaprasad <sajithar@wso2.com>
2017-11-10 17:27:36 +0530

    remove unnecessary default constructor from DashboardConfigurations

--- components/dashboards/org.wso2.carbon.dashboards.core/src/main/java/org/wso2/carbon/dashboards/core/bean/DashboardConfigurations.java
+++ components/dashboards/org.wso2.carbon.dashboards.core/src/main/java/org/wso2/carbon/dashboards/core/bean/DashboardConfigurations.java
@@ -1,22 +1,20 @@
 /*
- *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 package org.wso2.carbon.dashboards.core.bean;
 
 import org.wso2.carbon.config.annotation.Configuration;
@@ -26,21 +24,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Configuration bean class for dashboard configurations.
+ * Configuration bean class for dashboard.
+ *
+ * @since 4.0.0
  */
-@Configuration(namespace = "wso2.dashboard", description = "WSO2 Dashboard configuration object")
+@Configuration(namespace = "wso2.dashboard", description = "WSO2 Dashboard configurations")
 public class DashboardConfigurations {
-    @Element(description = "Database query map")
-    private Map<String, Map<String, String>> queries = new HashMap<>();
 
-    public DashboardConfigurations() {
-    }
+    @Element(description = "Map of database queries")
+    private Map<String, Map<String, String>> queries = new HashMap<>();
 
     public Map<String, Map<String, String>> getQueries() {
         return queries;
-    }
-
-    public void setQueries(Map<String, Map<String, String>> queries) {
-        this.queries = queries;
     }
 }
