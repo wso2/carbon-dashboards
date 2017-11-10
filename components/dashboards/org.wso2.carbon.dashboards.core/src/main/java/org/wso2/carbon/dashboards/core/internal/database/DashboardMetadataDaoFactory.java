0fe4249d3e32dfe8f81ca5b24190bdf4884a8190
Sajith Janaprasad <sajithar@wso2.com>
2017-11-10 11:14:12 +0530

    add DashboardMetadataDaoFactory class

---
+++ components/dashboards/org.wso2.carbon.dashboards.core/src/main/java/org/wso2/carbon/dashboards/core/internal/database/DashboardMetadataDaoFactory.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.dashboards.core.internal.database;
+
+import org.wso2.carbon.config.provider.ConfigProvider;
+import org.wso2.carbon.dashboards.core.exception.DashboardException;
+import org.wso2.carbon.datasource.core.api.DataSourceService;
+import org.wso2.carbon.datasource.core.exception.DataSourceException;
+
+import javax.sql.DataSource;
+
+/**
+ * Factory for {@link DashboardMetadataDAO} class.
+ *
+ * @since 4.0.0
+ */
+public class DashboardMetadataDaoFactory {
+
+    private static final String DATA_SOURCE_NAME_DASHBOARD = "WSO2_DASHBOARD_DB";
+
+    /**
+     * Creates a new DAO.
+     *
+     * @param dataSourceService data sources service
+     * @param configProvider    config provider
+     * @return DAO
+     * @throws DashboardException if cannot find required data source
+     */
+    public static DashboardMetadataDAO createDao(DataSourceService dataSourceService, ConfigProvider configProvider)
+            throws DashboardException {
+        DataSource dataSource;
+        try {
+            dataSource = (DataSource) dataSourceService.getDataSource(DATA_SOURCE_NAME_DASHBOARD);
+        } catch (DataSourceException e) {
+            throw new DashboardException("Cannot find data source named '" + DATA_SOURCE_NAME_DASHBOARD + "'.", e);
+        }
+        QueryManager queryManager = new QueryManager(configProvider);
+        return new DashboardMetadataDAO(dataSource, queryManager);
+    }
+}
