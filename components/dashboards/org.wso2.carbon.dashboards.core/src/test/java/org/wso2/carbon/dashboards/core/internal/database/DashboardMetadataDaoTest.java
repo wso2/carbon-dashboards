02b5c73cb1ecc67a03a444691f7e58dd8f538b01
Sajith Janaprasad <sajithar@wso2.com>
2017-11-17 11:22:41 +0530

    add tests for get, getAll, add, update, & delete in DashboardMetadataDao

--- components/dashboards/org.wso2.carbon.dashboards.core/src/test/java/org/wso2/carbon/dashboards/core/internal/database/DashboardMetadataDaoTest.java
+++ components/dashboards/org.wso2.carbon.dashboards.core/src/test/java/org/wso2/carbon/dashboards/core/internal/database/DashboardMetadataDaoTest.java
@@ -18,19 +18,30 @@
 
 package org.wso2.carbon.dashboards.core.internal.database;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.wso2.carbon.dashboards.core.bean.DashboardConfigurations;
+import org.wso2.carbon.dashboards.core.bean.DashboardMetadata;
+import org.wso2.carbon.dashboards.core.exception.DashboardException;
 
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.stream.Stream;
+import javax.sql.DataSource;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Test cases for {@link DashboardMetadataDao} class.
@@ -87,5 +98,180 @@ public class DashboardMetadataDaoTest {
         return Stream.of(
                 null, mock(Connection.class), exceptionThrowingConnection
         );
+    }
+
+    @Test
+    void testGetThrowException() throws Exception {
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        when(preparedStatement.executeQuery()).thenThrow(SQLException.class);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        Assertions.assertThrows(DashboardException.class, () -> dao.get("foo"));
+        verify(preparedStatement).close();
+        verify(connection).close();
+    }
+
+    @Test
+    void testGetWhenNoResult() throws Exception {
+        ResultSet resultSet = mock(ResultSet.class);
+        when(resultSet.next()).thenReturn(false);
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        Assertions.assertFalse(dao.get("foo").isPresent());
+        verify(preparedStatement).close();
+        verify(connection).close();
+        verify(resultSet).close();
+    }
+
+    @Test
+    void testGet() throws Exception {
+        Blob blob = mock(Blob.class);
+        when(blob.getBytes(anyLong(), anyInt())).thenReturn(new byte[]{});
+        ResultSet resultSet = mock(ResultSet.class);
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getBlob(anyString())).thenReturn(blob);
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        Assertions.assertTrue(dao.get("foo").isPresent());
+        verify(preparedStatement).close();
+        verify(connection).close();
+        verify(resultSet).close();
+    }
+
+    @Test
+    void testGetAllThrowException() throws Exception {
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        when(preparedStatement.executeQuery()).thenThrow(SQLException.class);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        Assertions.assertThrows(DashboardException.class, dao::getAll);
+        verify(preparedStatement).close();
+        verify(connection).close();
+    }
+
+    @Test
+    void testGetAll() throws Exception {
+        ResultSet resultSet = mock(ResultSet.class);
+        when(resultSet.next()).thenReturn(true, false);
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        Assertions.assertEquals(1, dao.getAll().size());
+        verify(preparedStatement).close();
+        verify(connection).close();
+        verify(resultSet).close();
+    }
+
+    @Test
+    void testAddThrowException() throws Exception {
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        when(preparedStatement.executeUpdate()).thenThrow(SQLException.class);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        Assertions.assertThrows(DashboardException.class, () -> dao.add(createDashboardMetadata()));
+        verify(connection).setAutoCommit(false);
+        verify(connection).rollback();
+        verify(preparedStatement).close();
+        verify(connection).close();
+    }
+
+    @Test
+    void testAdd() throws Exception {
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        dao.add(createDashboardMetadata());
+        verify(connection).setAutoCommit(false);
+        verify(preparedStatement).executeUpdate();
+        verify(connection).commit();
+        verify(preparedStatement).close();
+        verify(connection).close();
+    }
+
+    @Test
+    void testUpdateThrowException() throws Exception {
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        when(preparedStatement.executeUpdate()).thenThrow(SQLException.class);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        Assertions.assertThrows(DashboardException.class, () -> dao.update(createDashboardMetadata()));
+        verify(connection).setAutoCommit(false);
+        verify(connection).rollback();
+        verify(preparedStatement).close();
+        verify(connection).close();
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        dao.update(createDashboardMetadata());
+        verify(connection).setAutoCommit(false);
+        verify(preparedStatement).executeUpdate();
+        verify(connection).commit();
+        verify(preparedStatement).close();
+        verify(connection).close();
+    }
+
+    @Test
+    void testDeleteThrowException() throws Exception {
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        when(preparedStatement.executeUpdate()).thenThrow(SQLException.class);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        Assertions.assertThrows(DashboardException.class, () -> dao.delete("foo"));
+        verify(connection).setAutoCommit(false);
+        verify(connection).rollback();
+        verify(preparedStatement).close();
+        verify(connection).close();
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        Connection connection = createConnection(preparedStatement);
+        DashboardMetadataDao dao = createDao(connection);
+
+        dao.delete("foo");
+        verify(connection).setAutoCommit(false);
+        verify(preparedStatement).executeUpdate();
+        verify(connection).commit();
+        verify(preparedStatement).close();
+        verify(connection).close();
+    }
+
+    private static DashboardMetadataDao createDao(Connection mockedConnection) throws SQLException {
+        DataSource dataSource = mock(DataSource.class);
+        when(dataSource.getConnection()).thenReturn(mockedConnection);
+        return new DashboardMetadataDao(dataSource, new QueryProvider(new DashboardConfigurations()));
+    }
+
+    private static Connection createConnection(PreparedStatement mockPreparedStatement) throws SQLException {
+        Connection connection = mock(Connection.class);
+        when(connection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
+        when(connection.createBlob()).thenReturn(mock(Blob.class));
+        return connection;
+    }
+
+    private static DashboardMetadata createDashboardMetadata() {
+        DashboardMetadata dashboardMetadata = new DashboardMetadata();
+        dashboardMetadata.setPages("{}");
+        return dashboardMetadata;
     }
 }
