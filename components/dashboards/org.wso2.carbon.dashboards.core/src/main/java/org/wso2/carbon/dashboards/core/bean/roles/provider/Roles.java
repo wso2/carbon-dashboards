947d17dec780d04bab04226433a741d7c0cf4828
nisalaniroshana <nisalaniroshana@gmail.com>
2017-11-28 19:34:10 +0530

    Fix issue loading roles in Dasboard Settings page

---
+++ components/dashboards/org.wso2.carbon.dashboards.core/src/main/java/org/wso2/carbon/dashboards/core/bean/roles/provider/Roles.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.dashboards.core.bean.roles.provider;
+
+/**
+ * Created by nisala on 11/27/17.
+ */
+public class Roles {
+}
---
+++ components/dashboards/org.wso2.carbon.dashboards.core/src/main/java/org/wso2/carbon/dashboards/core/internal/roles/provider/RolesProvider.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.dashboards.core.internal.roles.provider;
+
+/**
+ * Created by nisala on 11/27/17.
+ */
+public class RolesProvider {
+}
