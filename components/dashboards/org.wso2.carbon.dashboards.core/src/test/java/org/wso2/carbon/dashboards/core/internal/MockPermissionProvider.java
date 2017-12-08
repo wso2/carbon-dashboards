82a49003a79713b080ae03575ddd4aa653132760
nisalaniroshana <nisalaniroshana@gmail.com>
2017-12-08 15:13:05 +0530

    Incooperate common analytics changes

--- components/dashboards/org.wso2.carbon.dashboards.core/src/test/java/org/wso2/carbon/dashboards/core/internal/MockPermissionProvider.java
+++ components/dashboards/org.wso2.carbon.dashboards.core/src/test/java/org/wso2/carbon/dashboards/core/internal/MockPermissionProvider.java
@@ -2,6 +2,7 @@ package org.wso2.carbon.dashboards.core.internal;
 
 import org.wso2.carbon.analytics.permissions.PermissionProvider;
 import org.wso2.carbon.analytics.permissions.bean.Permission;
+import org.wso2.carbon.analytics.permissions.bean.PermissionString;
 import org.wso2.carbon.analytics.permissions.bean.Role;
 import org.wso2.carbon.analytics.permissions.exceptions.PermissionException;
 
@@ -18,7 +19,18 @@ public class MockPermissionProvider implements PermissionProvider {
     }
 
     @Override
+    public String addPermissionAPI(Permission permission) throws PermissionException {
+        // TODO: 11/16/17 Need to implement in-memory permission store.
+        return new String();
+    }
+
+    @Override
     public void deletePermission(Permission permission) throws PermissionException {
+        // TODO: 11/16/17 Need to implement in-memory permission store.
+    }
+
+    @Override
+    public void deletePermission(String permissionID) throws PermissionException {
         // TODO: 11/16/17 Need to implement in-memory permission store.
     }
 
@@ -33,7 +45,17 @@ public class MockPermissionProvider implements PermissionProvider {
     }
 
     @Override
+    public void revokePermission(String permissionID) throws PermissionException {
+        // TODO: 11/16/17 Need to implement in-memory permission store.
+    }
+
+    @Override
     public void revokePermission(Permission permission, Role role) throws PermissionException {
+        // TODO: 11/16/17 Need to implement in-memory permission store.
+    }
+
+    @Override
+    public void revokePermission(Permission permission, String roleID) throws PermissionException {
         // TODO: 11/16/17 Need to implement in-memory permission store.
     }
 
@@ -44,8 +66,33 @@ public class MockPermissionProvider implements PermissionProvider {
     }
 
     @Override
+    public boolean hasPermission(String s, String permissionID) throws PermissionException {
+        // TODO: 11/16/17 Need to implement in-memory permission store.
+        return false;
+    }
+
+    @Override
     public List<Role> getGrantedRoles(Permission permission) throws PermissionException {
         // TODO: 11/16/17 Need to implement in-memory permission store.
         return new ArrayList<>();
     }
+
+    @Override
+    public List<Role> getGrantedRoles(String permissionID) throws PermissionException {
+        // TODO: 11/16/17 Need to implement in-memory permission store.
+        return new ArrayList<>();
+    }
+
+    @Override
+    public boolean isPermissionExists(Permission permission) throws PermissionException {
+        // TODO: 12/8/17 Need to implement in-memory permission store.
+        return false;
+    }
+
+    @Override
+    public List<PermissionString> getPermissionStrings(String appName) {
+        // TODO: 12/8/17 Need to implement in-memory permission store.
+        return new ArrayList<>();
+    }
 }
+
