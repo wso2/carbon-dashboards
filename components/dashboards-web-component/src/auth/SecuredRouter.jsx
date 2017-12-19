dcf069f3ba387dd5413b246fea9c8aa2867f8973
lasanthaS <lasn1987@gmail.com>
2017-12-19 20:35:51 +0530

    Reduce the timeout skew to 60 secs.

--- components/dashboards-web-component/src/auth/SecuredRouter.jsx
+++ components/dashboards-web-component/src/auth/SecuredRouter.jsx
@@ -55,7 +55,7 @@ export default class SecuredRouter extends Component {
                     AuthManager.authenticateWithRefreshToken();
                 }
             }
-        }, 600000);
+        }, 60000);
     }
     /**
      * Render routing.
