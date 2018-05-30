80cac034f1486544c633986d636fdc5c0ee5d1b7
Sajith Janaprasad <sajithar@wso2.com>
2018-05-30 16:25:38 +0530

    Add DashboardDesignerPage.

---
+++ components/dashboards-web-component/src/designer/DashboardDesignerPage.jsx
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import _ from 'lodash';
+
+import { Snackbar } from 'material-ui';
+import { MuiThemeProvider } from 'material-ui/styles';
+
+import Header from './components/Header';
+import LeftSideActions from './components/LeftSideActions';
+import PageLoadingIndicator from '../common/PageLoadingIndicator';
+import Error404 from '../error-pages/Error404';
+import Error403 from '../error-pages/Error403';
+import Error500 from '../error-pages/Error500';
+import defaultTheme from '../utils/Theme';
+import { HttpStatus } from '../utils/Constants';
+import DashboardAPI from '../utils/apis/DashboardAPI';
+
+export default class DashboardDesignerPage extends Component {
+    constructor(props) {
+        super(props);
+        this.dashboard = null;
+        this.state = {
+            dashboardUpdateResultMessage: null,
+            dashboardFetchStatus: HttpStatus.UNKNOWN,
+        };
+
+        this.fetchDashboard = this.fetchDashboard.bind(this);
+        this.handleDashboardUpdate = this.handleDashboardUpdate.bind(this);
+    }
+
+    componentDidMount() {
+        if (!this.dashboard) {
+            this.fetchDashboard();
+        }
+    }
+
+    fetchDashboard() {
+        new DashboardAPI('designer').getDashboardByID(this.props.match.params.dashboardId)
+            .then((response) => {
+                this.dashboard = response.data;
+                if (_.isString(this.dashboard.pages)) {
+                    this.dashboard.pages = JSON.parse(this.dashboard.pages);
+                }
+                this.setState({ dashboardFetchStatus: HttpStatus.OK });
+            })
+            .catch((error) => {
+                this.dashboard = null;
+                this.setState({ dashboardFetchStatus: error.response.status });
+            });
+    }
+
+    handleDashboardUpdate() {
+        return new Promise((resolve, reject) => {
+            new DashboardAPI('designer').updateDashboardByID(this.dashboard.url, this.dashboard)
+                .then(() => {
+                    this.setState({ dashboardUpdateResultMessage: 'Dashboard updated successfully.' });
+                    resolve();
+                })
+                .catch(() => {
+                    this.setState({ dashboardUpdateResultMessage: 'Cannot update dashboard!' });
+                    reject();
+                });
+        });
+    }
+
+    renderHeader(theme) {
+        return (
+            <Header
+                theme={theme}
+                title={<FormattedMessage id="dashboard.designer.title" defaultMessage="Dashboard Designer" />}
+            />
+        );
+    }
+
+    render() {
+        switch (this.state.dashboardFetchStatus) {
+            case HttpStatus.NOTFOUND:
+                return <Error404 />;
+            case HttpStatus.FORBIDDEN:
+                return <Error403 />;
+            case HttpStatus.SERVE_ERROR:
+                return <Error500 />;
+            case HttpStatus.UNKNOWN:
+                // Still fetching the dashboard.
+                return (
+                    <MuiThemeProvider muiTheme={defaultTheme}>
+                        {this.renderHeader(defaultTheme)}
+                        <PageLoadingIndicator />
+                    </MuiThemeProvider>
+                );
+            default:
+            // Fetched the dashboard successfully.
+        }
+        // No page is mentioned in the URl. Let's redirect to the landing page so that we have a page to render.
+        if (!this.props.match.params.pageId) {
+            console.log(`Redirecting to landing page as pageId=${this.props.match.params.pageId}`);
+            return <Redirect to={`/designer/${this.dashboard.url}/${this.dashboard.landingPage}`} />;
+        }
+
+        return (
+            <MuiThemeProvider muiTheme={defaultTheme}>
+                <span>
+                    {this.renderHeader(defaultTheme)}
+                    <div style={{ width: '100%', height: '100%' }}>
+                        <LeftSideActions
+                            theme={defaultTheme}
+                            dashboard={this.dashboard}
+                            updateDashboard={this.handleDashboardUpdate}
+                        />
+                    </div>
+                    <Snackbar
+                        open={this.state.dashboardUpdateResultMessage != null}
+                        message={this.state.dashboardUpdateResultMessage}
+                        autoHideDuration={4000}
+                        onRequestClose={() => this.setState({ dashboardUpdateResultMessage: null })}
+                    />
+                </span>
+            </MuiThemeProvider>
+        );
+    }
+}
