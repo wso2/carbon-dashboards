e0939b11f5b50975fa897ef7e660056f37e35a38
Sajith Janaprasad <sajithar@wso2.com>
2018-06-06 06:36:19 +0530

    Fix logic in generateguid in DashboardUtils.

--- components/dashboards-web-component/src/utils/DashboardUtils.jsx
+++ components/dashboards-web-component/src/utils/DashboardUtils.jsx
@@ -22,13 +22,11 @@ export default class DashboardUtils {
     }
 
     static generateguid() {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-                .toString(16)
-                .substring(1);
-        }
-
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-            s4() + '-' + s4() + s4() + s4();
+        /* eslint-disable no-bitwise, no-mixed-operators */
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+            const r = Math.random() * 16 | 0;
+            const v = (c === 'x') ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
     }
 }
