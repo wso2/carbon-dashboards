e9204f1eddf6da269761ed60a884cbc1a5c876d7
kts-desilva <ktsdesilva.15@cse.mrt.ac.lk>
2018-08-21 09:38:46 +0530

    Resolve merge conflicts.

--- components/dashboards-web-component/src/viewer/DashboardViewPage.jsx
+++ components/dashboards-web-component/src/viewer/DashboardViewPage.jsx
@@ -22,9 +22,11 @@ import { Redirect, withRouter } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import _ from 'lodash';
 
-import { Divider, Drawer, IconButton, List, ListItem, makeSelectable, Subheader, Toggle } from 'material-ui';
+import { Divider, Drawer, IconButton, List, ListItem, makeSelectable, Subheader, Toggle, Checkbox, CardText, CardHeader,
+         CardActions ,RaisedButton, SelectField, MenuItem, Card, FlatButton, Dialog, CircularProgress } from 'material-ui';
 import { MuiThemeProvider } from 'material-ui/styles';
 import { ActionHome, NavigationMenu } from 'material-ui/svg-icons';
+import DeleteIcon from 'material-ui/svg-icons/action/delete';
 
 import DashboardAPI from '../utils/apis/DashboardAPI';
 import Error403 from '../error-pages/Error403';
@@ -40,6 +42,11 @@ import ThemeButton from './components/ThemeButton';
 import { darkTheme, lightTheme } from '../utils/Theme';
 import '../utils/GoldenLayoutOverrides.css';
 
+import jspdf from 'jspdf';
+import html2cavas from 'html2canvas';
+import axios from 'axios';
+import { pdfConfig } from './components/JspdfConf.js';
+
 const SelectableList = makeSelectable(List);
 
 class DashboardViewPage extends Component {
@@ -53,6 +60,17 @@ class DashboardViewPage extends Component {
             dashboardFetchStatus: HttpStatus.UNKNOWN,
             isSidePaneOpen: true,
             isCurrentThemeDark: isDarkTheme ? isDarkTheme === 'true' : true,
+            printEnabledList: [],
+            pageList: [],
+            isPrintChecked: false,
+            pageSize: "A4",
+            resizeRatio: 1,
+            canvasWidth: 0,
+            canvasHeight: 0,
+            expanded: false,
+            reportStatusOpen: false,
+            completed: 0,
+            dialogOpen: false,
         };
 
         this.fetchDashboard = this.fetchDashboard.bind(this);
@@ -63,6 +81,15 @@ class DashboardViewPage extends Component {
         this.renderSidePane = this.renderSidePane.bind(this);
         this.renderPagesList = this.renderPagesList.bind(this);
         this.renderDashboard = this.renderDashboard.bind(this);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.capturePage = this.capturePage.bind(this);
+        this.generatePdf = this.generatePdf.bind(this);
+        this.handleCardClick = this.handleCardClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleOpen = this.handleOpen.bind(this);
+        this.handleincludeGenerateTime = this.handleincludeGenerateTime.bind(this);
+        this.removePage = this.removePage.bind(this);
     }
 
     componentDidMount() {
@@ -136,6 +163,25 @@ class DashboardViewPage extends Component {
             setTimeout(() => this.handleSidePaneToggle(), 1000);
         }
 
+        const dialogActions = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onClick={this.handleClose}
+            />,
+            <FlatButton
+                label="Export"
+                primary={true}
+                onClick={this.generatePdf}
+            />,
+        ];
+
+        const customStatusDailogStyle = {
+            width: 400,
+            maxWidth: 'none',
+            position: 'relative'
+        };
+
         return (
             <MuiThemeProvider muiTheme={currentTheme}>
                 {this.renderHeader(currentTheme)}
@@ -153,6 +199,34 @@ class DashboardViewPage extends Component {
                 >
                     {this.renderDashboard(currentTheme)}
                 </div>
+
+                <Dialog
+                    title="Select PDF options"
+                    actions={dialogActions}
+                    modal={true}
+                    open={this.state.dialogOpen}
+                >
+                    <Checkbox
+                        label='Include report genetation time'
+                        onClick={this.handleincludeGenerateTime}
+                    />
+                </Dialog>
+
+                <Dialog
+                    title="Report is generating"
+                    contentStyle={customStatusDailogStyle}
+                    modal={true}
+                    open={this.state.reportStatusOpen}
+                >
+                    <CircularProgress
+                        mode="determinate"
+                        value={this.state.completed}
+                        size={80}
+                        thickness={5}
+                        style={{ marginLeft: '25%' }}
+                    />
+                </Dialog>
+
             </MuiThemeProvider>
         );
     }
@@ -186,6 +260,9 @@ class DashboardViewPage extends Component {
     renderSidePane(theme) {
         const pageId = this.props.match.params.pageId,
             subPageId = this.props.match.params.subPageId;
+
+        const pageSizes = ["A4", "Letter", "Government-Letter"];
+
         return (
             <Drawer
                 docked={false}
@@ -200,8 +277,77 @@ class DashboardViewPage extends Component {
                 <SelectableList value={subPageId ? `${pageId}/${subPageId}` : pageId}>
                     {this.renderPagesList()}
                 </SelectableList>
+                <br />
+                <Divider />
+                <span>
+                    <Card
+                        style={{ margin: 10 }}
+                        expanded={this.state.expanded}
+                        onExpandChange={this.handleCardClick}
+                    >
+                        <CardHeader title='Export dashboard' actAsExpander style={{ paddingRight: '0px' }} />
+                        <CardActions expandable style={{ display: 'flex', paddingRight: '0px' }}>
+                            <div style={{ marginRight: 0 }}>
+
+
+                                <RaisedButton label='Capture current page'
+                                    onClick={this.capturePage}
+                                    primary />
+
+                                <List>
+                                    {this.state.pageList.map(field =>
+                                        <ListItem primaryText={
+                                                        this.dashboard.pages.find(page => page.id === field).name}
+                                        rightIcon={<DeleteIcon />}
+                                                  onClick={() => this.removePage(field)} />
+                                    )}
+                                </List>
+
+                                <SelectField
+                                    style={{ width: 200 }}
+                                    floatingLabelText='Page Size'
+                                    value={this.state.pageSize}
+                                    onChange={(event, index, value) => { this.setState({ pageSize: value }) }}
+
+                                >
+                                    {pageSizes.map(field =>
+                                        (
+                                            <MenuItem
+                                                key={field}
+                                                value={field}
+                                                primaryText={field}
+                                            />
+                                        ))}
+                                </SelectField>
+
+                                <RaisedButton label='Export'
+                                    onClick={this.handleOpen}
+                                    disabled={!(this.state.pageList.length > 0)}
+                                    primary />
+                            </div>
+                        </CardActions>
+                    </Card>
+                </span>
+
             </Drawer>
         );
+    }
+
+    handleCardClick(expand) {
+        this.setState({ expanded: expand });
+    }
+
+    handleChange(index) {
+        let newArray = this.state.printEnabledList.slice();
+
+        if (typeof newArray[index] != 'undefined') {
+            newArray[index].value = !newArray[index].value;
+            this.setState({ printEnabledList: newArray }, () => this.capturePage(index));
+        } else {
+            newArray[index] = { value: true };
+            this.setState({ printEnabledList: newArray }, () => this.capturePage(index));
+        }
+
     }
 
     /**
@@ -272,6 +418,234 @@ class DashboardViewPage extends Component {
             return <Redirect to={this.getNavigationToPage(this.dashboard.landingPage)} />;
         }
     }
+
+    handleClose() {
+        this.setState({ dialogOpen: false });
+    }
+
+    handleOpen() {
+        this.setState({ dialogOpen: true });
+    }
+
+    handleincludeGenerateTime() {
+        this.setState({ includeTime: !this.state.includeTime });
+    }
+
+    handleReportStatusClose() {
+        this.setState({ reportStatusOpen: false });
+    }
+
+    handleReportStatusOpen() {
+        this.setState({ reportStatusOpen: true });
+    }
+
+    progress = () => {
+        const { completed } = this.state;
+        this.setState({ completed: completed >= 100 ? 0 : completed + 10 });
+    };
+
+    formatDate(date) {
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        day = day < 10 ? '0' + day : day;
+        month = month < 10 ? '0' + month : month;
+        const timeStr = hours + ':' + minutes + ' ' + ampm;
+        return day + "/" + month + "/" + date.getFullYear() + "  " + timeStr;
+    }
+
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async generatePdf() {
+
+        //Starts showing report generation progress
+        this.handleClose();
+        this.handleReportStatusOpen();
+        this.timer = setInterval(this.progress, 195);
+
+        let pdf = new jspdf("l", "px", this.state.pageSize.toLowerCase());
+
+        const title = document.getElementsByTagName("h1")[0].innerText;
+
+        // Get header : a cutomized header defined by the user or else the default stream processor header
+        await this.addPdfImage("/portal/apis/dashboards/pdfHeader", function (res) {
+            if (res != 'none') {
+                localStorage.setItem("dashboardHeader", res);
+            }
+        });
+
+        // Get footer : a cutomized footer defined by the user or else the no footer by default
+        await this.addPdfImage("/portal/apis/dashboards/pdfFooter", function (res) {
+            if (res != 'none') {
+                localStorage.setItem("dashboardFooter", res);
+            }
+        });
+
+        // To handle the async calls (wait until the image data is stored in localStorage)
+        await this.sleep(2000);
+
+        const img = localStorage.getItem("dashboardHeader");
+        pdf.addImage(img, "JPEG", pdfConfig.stampImageDashboard.coordinates.x,
+                     pdfConfig.stampImageDashboard.coordinates.y, pdfConfig.stampImageDashboard.size.x,
+                     pdfConfig.stampImageDashboard.size.y);
+
+        this.state.pageList.map((item, ind) => {
+            const it = localStorage.getItem(item);
+            const image = JSON.parse(it);
+            const imgData = image.imageData;
+
+            const imgWidth = image.width;
+            const imgHeight = image.height;
+
+            const page = this.dashboard.pages.find(page => {
+                return page.id == item;
+            })
+
+            //Add title
+            pdf.setFontSize(15);
+            pdf.setFontType('bold');
+            const padding = Array.apply(null, Array(title.length + 4)).map(function () { return "  " })
+            pdf.text((pdf.internal.pageSize.getHeight() / 2 + 30), 18, title + " : ");
+
+            const ntext = padding.join("") + page.name;
+            pdf.setFontType('normal');
+            pdf.text((pdf.internal.pageSize.getHeight() / 2 + 30), 18, ntext);
+
+            let pdfInfo = "";
+
+            if (this.state.includeTime) {
+                pdfInfo = "Generated on : " + this.formatDate(new Date());
+            }
+
+            pdf.setFontType('bold');
+            pdf.setFontSize(12);
+            pdf.text(pdfInfo, pdfConfig.stampImageDashboard.coordinates.x, 35);
+            pdf.setFontType('normal');
+
+            if (imgWidth > imgHeight) {
+                const val = (pdf.internal.pageSize.getWidth()) / imgWidth;
+
+                if (imgHeight * val < (pdf.internal.pageSize.getHeight())) {
+                    pdf.addImage(imgData, 'JPEG', 0, 55, imgWidth * val, imgHeight * val, "dashboard" + ind, "FAST");
+                } else {
+                    const valH = (pdf.internal.pageSize.getHeight()) / imgHeight;
+                    pdf.addImage(imgData, 'JPEG', 0, 55, imgWidth * valH, imgHeight * valH, "dashboard" + ind, "FAST");
+                }
+
+            } else {
+                const val = (pdf.internal.pageSize.getHeight()) / imgHeight;
+                pdf = new jspdf("p", "px", "a4");
+                pdf.addImage(imgData, 'JPEG', 0, 35, imgWidth * val, imgHeight * val, "dashboard" + ind, "FAST");
+            }
+
+            // FOOTER : a customized footer defined by the user in the deployment.yaml file or else no footer by default
+            const str = "Page " + (ind + 1) + " of " + this.state.pageList.length;
+            pdf.setFontSize(10);
+            const pageHeight = pdf.internal.pageSize.height || pdf.internal.pageSize.getHeight();
+            pdf.text(str, 10, pageHeight - 10);
+
+            const footerImg = localStorage.getItem('dashboardFooter');
+            if (footerImg != null) {
+                pdf.addImage(footerImg, "JPEG", 380, 780, pdfConfig.stampImage.size.x, pdfConfig.stampImage.size.y);
+            }
+
+            if (ind < this.state.pageList.length - 1) {
+                pdf.addPage();
+
+                // HEADER : a cutomized header defined by the user or else the default stream processor header
+                const headerImg = localStorage.getItem('dashboardHeader');
+                if (headerImg != null) {
+                    pdf.addImage(headerImg, "JPEG", pdfConfig.stampImageDashboard.coordinates.x,
+                                 pdfConfig.stampImageDashboard.coordinates.y, pdfConfig.stampImageDashboard.size.x,
+                                 pdfConfig.stampImageDashboard.size.y);
+                }
+            }
+        });
+
+        clearInterval(this.timer);
+        this.handleReportStatusClose();
+        this.state.completed = 0;
+        this.state.includeTime = false;
+
+        const docName = title + ".pdf";
+        pdf.save(docName);
+
+    }
+
+    // Get the image from deployment.yaml file through the server
+    async addPdfImage(url, callback) {
+        let path = "/portal/public/app/images/"
+
+        await axios.get(url, { auth: { username: "admin", password: "admin" } })
+            .then(function (res) {
+                if (res.data === '') {
+                    callback('none');
+                } else {
+                    path += res.data;
+
+                    //Convert image to bdase64 encoded image (data_url)
+                    const canvas = document.createElement('canvas');
+                    const ctx = canvas.getContext('2d');
+                    const img = new Image();
+                    let imgStr;
+
+                    img.onload = function () {
+                        canvas.width = img.width;
+                        canvas.height = img.height;
+                        ctx.drawImage(img, 0, 0);
+                        imgStr = canvas.toDataURL("image/png");
+                        callback(imgStr);
+                    }
+
+                    img.src = path;
+                }
+
+            });
+    }
+
+    async removePage(index) {
+
+        const newAr = this.state.pageList.slice();
+        const ind = newAr.indexOf(index);
+        newAr.splice(ind, 1);
+        this.setState({ pageList: newAr });
+
+        if (newAr.indexOf(index) == -1) {
+            localStorage.removeItem(index);
+        }
+
+    }
+
+    async capturePage(index) {
+
+        const url = window.location.href;
+        const parts = url.split('/');
+        const currentPage = parts[parts.length - 1] === "" ? parts[parts.length - 2] : parts[parts.length - 1];
+
+        await html2cavas(document.getElementById("dashboard-container")).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            const w = canvas.width;
+            const h = canvas.height;
+            localStorage.setItem(currentPage, JSON.stringify({ imageData: imgData, width: w, height: h }));
+
+            const newAr = this.state.pageList.slice();
+            newAr.push(currentPage);
+            this.setState({ pageList: newAr });
+
+            const val = 620 / canvas.width;
+            this.state.resizeRatio = val;
+            this.state.canvasWidth = canvas.width;
+            this.state.canvasHeight = canvas.height;
+        });
+    }
+
 }
 
 export default withRouter(DashboardViewPage);
